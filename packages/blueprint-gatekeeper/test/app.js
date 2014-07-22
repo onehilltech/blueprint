95b4582898233212e0f63a6461027984f04eb5cd
hilljh82 <hilljh82@gmail.com>
2014-07-21 23:01:51 -0400

    Removed database connection logic since it was causing runtime error

--- packages/blueprint-gatekeeper/test/app.js
+++ packages/blueprint-gatekeeper/test/app.js
@@ -5,21 +5,6 @@ var express      = require ('express'),
     morgan       = require ('morgan'),
     config       = require ('./app-config');
 
-var app = express ();
-
-// Connect to the database. The database configuration comes from a 
-// configuration file. Setup the callback for different events on the 
-// connection.
-var connect = function () {
-  mongoose.connect (config.connstr, config.mongodb);
-}
-
-connect ();
-
-mongoose.connection.on ('error', function (err) {
-  console.log (err);
-});
-
 // Create the application, and set the default configuration.
 var app = express ();
 app.use (morgan (config.morgan));
