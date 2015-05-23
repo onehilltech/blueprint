c3b0d4765895e73df49d2ceb085e896a2d3fc983
hilljh82 <hilljh82@gmail.com>
2015-05-23 09:50:32 -0400

    Simplified implementation

--- packages/blueprint-gatekeeper/lib/router/oauth2/password.js
+++ packages/blueprint-gatekeeper/lib/router/oauth2/password.js
@@ -37,9 +37,7 @@ function PasswordRouter (opts, server) {
           return done (new oauth2orize.TokenError (err.message, 'invalid_client'));
 
         winston.log('info', 'client %s generating access/refresh token for %s', client.id, username);
-        AccessToken.generateAndSave (self._tokenLength, client.id, account.id, function (err, accessToken, refreshToken) {
-          return done (err, accessToken, refreshToken, {});
-        });
+        AccessToken.generateAndSave (self._tokenLength, client.id, account.id, done);
       });
     }));
 }
