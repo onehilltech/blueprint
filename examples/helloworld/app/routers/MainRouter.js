16504fe3d4d56a496ae76cf3b66ace8d894a7c11
hilljh82 <hilljh82@gmail.com>
2015-08-01 01:28:06 -0400

    Added support for router parameters

--- examples/helloworld/app/routers/MainRouter.js
+++ examples/helloworld/app/routers/MainRouter.js
@@ -1,4 +1,6 @@
 module.exports = exports = {
+  ':testParam' : 'HelloWorldController@lookupParam',
+
   '/helloworld' : {
     get  : { view   : 'helloworld' },
     post : { action : 'HelloWorldController@echoName'},
