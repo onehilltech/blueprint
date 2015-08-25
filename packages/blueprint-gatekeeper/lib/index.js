607767eb1f0c945756cf5a19414e250e62d0b7ba
hilljh82 <hilljh82@gmail.com>
2015-08-24 22:28:48 -0400

    Specified the app path incorrectly

--- packages/blueprint-gatekeeper/lib/index.js
+++ packages/blueprint-gatekeeper/lib/index.js
@@ -1,9 +1,11 @@
 var blueprint = require ('blueprint')
+  , path      = require ('path')
   ;
 
 // Export the application as a module. This allows other applications to integrate
 // our application logic into their application logic.
-module.exports = exports = new blueprint.ApplicationModule ('../app');
+var appPath = path.resolve (__dirname, '../app');
+module.exports = exports = new blueprint.ApplicationModule (appPath);
 
 // Export the authentication package.
 exports.auth = require ('./authentication');
