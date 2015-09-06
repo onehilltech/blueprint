dce189dc033875e3fdab41b0a8bf4cc7e7b4257b
hilljh82 <hilljh82@gmail.com>
2015-09-05 23:26:48 -0400

    Added https:// support to the example

--- packages/blueprint/examples/01_helloworld/app/configs/server.config.js
+++ packages/blueprint/examples/01_helloworld/app/configs/server.config.js
@@ -1,7 +1,17 @@
+var blueprint = require ('blueprint')
+  ;
+
 module.exports = exports = {
   protocols : {
     http : {
-      port : 5000
+      port: 5000
+    },
+    https : {
+      port : 5001,
+      options : {
+        key  : blueprint.app.resource ('helloworld-key.pem'),
+        cert : blueprint.app.resource ('helloworld-cert.pem')
+      }
     }
   },
 
