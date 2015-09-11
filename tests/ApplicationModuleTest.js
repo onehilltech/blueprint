235c20c21a90fd5bc351c4cabe5fb95ee671de73
hilljh82 <hilljh82@gmail.com>
2015-09-11 11:41:56 -0400

    Added more unit tests

--- tests/ApplicationModuleTest.js
+++ tests/ApplicationModuleTest.js
@@ -21,10 +21,24 @@ describe ('ApplicationModule', function () {
     });
   });
 
+  describe ('#listeners', function () {
+    it ('should return the loaded listeners', function () {
+      expect (appModule.listeners).to.have.property ('app.init');
+      expect (appModule.listeners['app.init']).to.have.keys (['TestListener', 'TargetListener']);
+      expect (appModule.listeners['app.init']['TargetListener']).to.have.property ('targetMessenger');
+    });
+  });
+
   describe ('#models', function () {
     it ('should return the loaded models', function () {
       expect (appModule.models).to.have.deep.property ('TestModel1')
       expect (appModule.models).to.have.deep.property ('inner.TestModel2');
+    });
+  });
+
+  describe ('#controllers', function () {
+    it ('should return the loaded controllers', function () {
+      expect (appModule.controllers).to.have.property ('TestController');
     });
   });
 });
