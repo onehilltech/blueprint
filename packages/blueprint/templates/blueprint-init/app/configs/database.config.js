9ce32689fe60d7d266c9573b89b202bfe06b393b
hilljh82 <hilljh82@gmail.com>
2015-10-14 22:07:16 -0400

    Added default templates

---
+++ packages/blueprint/templates/blueprint-init/app/configs/database.config.js
@@ -0,0 +1,16 @@
+module.exports = exports = {
+  options : {
+    db: {
+      native_parser: true,
+      read_preference: "primary",
+      forceServerObjectId: false,
+      w: 1
+    },
+    server: {
+      auto_reconnect: true,
+      keepAlive: 1,
+      poolSize: 5,
+      socketOptions: {}
+    }
+  }
+};
