2efbc69d9fc75ecdae888160463f067d50c2f1df
hilljh82 <hilljh82@gmail.com>
2015-10-17 17:14:06 -0400

    More fixes related to name change

--- examples/02_messageboard/app/configs/database.config.js
+++ examples/02_messageboard/app/configs/database.config.js
@@ -1,5 +1,5 @@
 module.exports = exports = {
-  connstr: "mongodb://localhost/blueprint-examples-messageboard",
+  connstr: "mongodb://localhost/xpression-examples-messageboard",
 
   options : {
     db: {
