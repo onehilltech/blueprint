e8b11caad558be2c431c024801c1a3b2d8419732
hilljh82 <hilljh82@gmail.com>
2015-10-19 09:56:01 -0400

    Testing if app is undefined

--- tests/fixtures/app/models/inner/TestModel2.js
+++ tests/fixtures/app/models/inner/TestModel2.js
@@ -1,8 +1,8 @@
 var xpression = require ('../../../xpression')
   ;
 
-var undef = xpression === undefined;
-console.log ('is undefined: ' + undef);
+var undef = xpression.app === undefined;
+console.log ('app is undefined: ' + undef);
 
 var schema = new xpression.Schema ({
   /// Username for the account.
