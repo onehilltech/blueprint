aa3b8b921888ce978c3048237cbfa3e57f57fe8e
hilljh82 <hilljh82@gmail.com>
2015-10-18 22:46:35 -0400

    Added unit test for library module

--- tests/lib/indexTest.js
+++ tests/lib/indexTest.js
@@ -1,7 +1,21 @@
-var expect = require ('chai').expect
-  , path   = require ('path')
+var expect    = require ('chai').expect
+  , xpression = require ('../../lib')
   ;
 
 describe ('index', function () {
+  describe ('#', function () {
+    it ('should have keys for events', function () {
+      var keys = [
+        'Application',
+        'ApplicationModule',
+        'BaseController',
+        'controller',
+        'emit',
+        'model',
+        'on'
+      ];
 
+      expect (xpression).to.have.keys (keys);
+    });
+  });
 });
