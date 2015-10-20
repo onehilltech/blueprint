2e0ea40573f3b29c47f020a784cb795acc532b3a
hilljh82 <hilljh82@gmail.com>
2015-10-20 16:18:21 -0400

    Added version to the Framework module

--- tests/lib/FrameworkTest.js
+++ tests/lib/FrameworkTest.js
@@ -18,4 +18,20 @@ describe ('Framework', function () {
       expect (framework).to.equal (process.mainModule.xpression);
     });
   });
+
+  describe ('destroy', function () {
+    it ('should destroy the framework', function () {
+      Framework.destroy ();
+      expect (process.mainModule.xpression).to.be.undefined;
+    });
+  });
+
+  describe ('version', function () {
+    it ('should equal the version package.json', function () {
+      var version = require ('../../package.json').version;
+
+      expect (Framework.version).to.not.be.undefined;
+      expect (Framework.version).to.equal (version);
+    });
+  });
 });
