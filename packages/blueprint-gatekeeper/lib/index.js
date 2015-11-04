90da6f3f78031763ab53ad401269cf22a3dbb3e4
James H. Hill <hilljh82@gmail.com>
2015-11-03 22:06:36 -0500

    Failed to load module correctly

--- packages/blueprint-gatekeeper/lib/index.js
+++ packages/blueprint-gatekeeper/lib/index.js
@@ -5,7 +5,7 @@ var blueprint = require ('@onehilltech/blueprint')
 // Export the application as a module. This allows other applications to integrate
 // our application logic into their application logic.
 var appPath = path.resolve (__dirname, '../app');
-module.exports = exports = new blueprint.ApplicationModule (appPath);
+module.exports = exports = blueprint.ApplicationModule.load (appPath);
 
 // Export the authentication package.
 exports.auth = require ('./authentication');
