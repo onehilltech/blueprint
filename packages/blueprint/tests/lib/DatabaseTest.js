c626f2681c519126a19c5d32ffd85d4925f4ccf7
James H. Hill <hilljh82@gmail.com>
2015-12-24 13:53:35 -0500

    Database is a wrapper around a Mongoose connection, not the default Mongoose connection

--- packages/blueprint/tests/lib/DatabaseTest.js
+++ packages/blueprint/tests/lib/DatabaseTest.js
@@ -1,74 +1,96 @@
 var expect    = require ('chai').expect
   , path      = require ('path')
   , fs        = require ('fs')
+  , async     = require ('async')
   ;
 
-var blueprint = require ('../fixtures/blueprint')
-  , Database  = require ('../../lib/Database')
+var Database  = require ('../../lib/Database')
   ;
 
 describe ('Database', function () {
-  var appPath = path.resolve (__dirname, '../fixtures/app');
   var db;
-  var schema;
+  var Model;
 
   const COLLECTION_NAME = 'message';
 
-  var opts = {
-    connstr: 'mongodb://localhost/blueprint-unit-tests',
-
-    options : {
-      db: {
-        native_parser: true,
-        read_preference: "primary",
-        forceServerObjectId: false,
-        w: 1
-      },
-      server: {
-        auto_reconnect: true,
-        keepAlive: 1,
-        poolSize: 5,
-        socketOptions: {}
-      }
-    }
-  };
-
-  before (function () {
-    blueprint.destroy ();
-    app = blueprint.Application (appPath);
-
-    schema = new blueprint.Schema({
-      timestamp: {type: Date, required: true, default: Date.now},
-      title: {type: String, required: true, trim: true},
-      content: {type: String, required: true, trim: true},
-    });
+  var schema  = new Database.Schema ({
+    first_name : {type: String, required: true},
+    last_name  : {type: String, required: true}
   });
 
   describe ('new Database', function () {
-    db = new Database (opts);
-
     it ('should create a new database', function () {
-      expect (db).to.not.be.null;
-    });
-  });
+      var opts = {
+        connstr: 'mongodb://localhost/blueprint_DatabaseTest',
 
-  describe ('#registerModel', function () {
-    it ('should register a model with the database', function () {
-      db.registerModel (COLLECTION_NAME, schema);
-      expect (db.models).to.have.any.keys (COLLECTION_NAME);
+        options : {
+          db: {
+            native_parser: true,
+            read_preference: "primary",
+            forceServerObjectId: false,
+            w: 1
+          },
+          server: {
+            auto_reconnect: true,
+            keepAlive: 1,
+            poolSize: 5,
+            socketOptions: {}
+          }
+        }
+      };
+
+      db = new Database (opts);
+      expect (db).to.not.be.null;
     });
   });
 
   describe ('#connect', function () {
     it ('should connect to the database', function (done) {
-      db.connect (done);
+      db.connect (function (err) {
+        if (err) return done (err);
+
+        expect (db._conn.readyState).to.equal (1);
+        return done ();
+      });
+    });
+  });
+
+  describe ('#registerModel', function () {
+    it ('should register a model with the database', function (done) {
+      Model = db.registerModel (COLLECTION_NAME, schema);
+      expect (db.models).to.have.keys (COLLECTION_NAME);
+
+      Model.remove ({}, function (err) {
+        return done (err);
+      });
     });
   });
 
   describe ('#seed', function () {
     it ('should seed the database', function (done) {
-      var seedPath = path.resolve (__dirname, '../fixtures/seeds');
-      db.seed (seedPath, done);
+      var seed = {
+        data : [
+          { first_name: 'John',  last_name: 'Doe'},
+          { first_name: 'Jane',  last_name: 'Doe'},
+          { first_name: 'Luke',  last_name: 'Skywalker'},
+          { first_name: 'Darth', last_name: 'Vader'}
+        ]
+      };
+
+      db.seed (COLLECTION_NAME, seed, function (err, seed) {
+        expect (err).to.be.undefined;
+        expect (seed).to.not.be.undefined;
+
+        expect (seed.documents).to.have.length (4);
+
+        Model.find ({}, function (err, docs) {
+          if (err) return done (err);
+
+          expect (docs).to.have.length (4);
+
+          return done ();
+        });
+      });
     });
   });
 
