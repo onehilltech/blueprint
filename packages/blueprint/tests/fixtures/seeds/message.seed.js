cec4db92533dbd5dc8149a9dfbbb22792acf0604
James H. Hill <hilljh82@gmail.com>
2015-12-24 13:53:51 -0500

    Deleted fixture file

--- packages/blueprint/tests/fixtures/seeds/message.seed.js
+++
@@ -1,8 +0,0 @@
-module.exports = exports = {
-  data : [
-    { firstname: 'John', lastname: 'Doe'},
-    { firstname: 'Jane', lastname: 'Doe'},
-    { firstname: 'Luke', lastname: 'Skywalker'},
-    { firstname: 'Darth', lastname: 'Vader'}
-  ]
-};
