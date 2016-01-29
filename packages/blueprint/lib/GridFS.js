7205d7c6c61188868cd33b80ca752303543aaa46
James H. Hill <hilljh82@gmail.com>
2016-01-29 17:00:28 -0500

    Added find() methods to GridFS

--- packages/blueprint/lib/GridFS.js
+++ packages/blueprint/lib/GridFS.js
@@ -45,8 +45,35 @@ GridFS.prototype.writeFileToDatabase = function (file, metadata, done) {
   writeStream.on ('close', done);
 };
 
+/**
+ * Read a file from the GridFS database.
+ *
+ * @param opts
+ * @returns {Stream}
+ */
 GridFS.prototype.createReadStream = function (opts) {
   return this._gridFS.createReadStream (opts);
+};
+
+/**
+ * Find metadata for a set of files.
+ *
+ * @param filter
+ * @param options
+ */
+GridFS.prototype.find = function (filter, options) {
+  this._gridFS.files.find (filter, options);
+};
+
+/**
+ * Find the metadata for a single file.
+ *
+ * @param options
+ * @param callback
+ * @returns {Query|*|Promise}
+ */
+GridFS.prototype.findOne = function (options, callback) {
+  return this._gridFS.findOne (options, callback);
 };
 
 exports = module.exports = GridFS;
