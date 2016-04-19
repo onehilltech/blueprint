b46006556c43451cee6ce9562492dc5758741efc
James H. Hill <hilljh82@gmail.com>
2016-04-19 10:03:22 -0400

    Simplified the configurations

--- packages/blueprint-gatekeeper/app/configs/server.config.js
+++ packages/blueprint-gatekeeper/app/configs/server.config.js
@@ -1,7 +1,27 @@
 module.exports = exports = {
+  protocols: {
+    http: {
+      port: 5000
+    }
+  },
+
   middleware : {
     validator  : { },
     bodyParser : { json : { } },
-    passport   : {  }
+    passport   : {  },
+
+    morgan: {
+      format: 'dev',
+      immediate: true
+    },
+
+    session: {
+      cookie: {
+        secure: false
+      },
+      secret: "123abc",
+      resave: true,
+      saveUninitialized: true
+    }
   }
 };
