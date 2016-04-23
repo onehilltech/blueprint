84dae6a096760055f4b089465b26f1f6ea20a60c
James H. Hill <hilljh82@gmail.com>
2016-04-22 22:19:14 -0400

    Subrouters in the dictionary are built with correct base paths; can nest routes inside router specification

--- tests/lib/RouterBuilderTest.js
+++ tests/lib/RouterBuilderTest.js
@@ -8,16 +8,35 @@ var ApplicationModule = require ('../../lib/ApplicationModule')
 
 describe ('RouterBuilder', function () {
   var routerBuilder;
+  var routersPath;
 
   describe ('new RouterBuilder ()', function () {
     var appModule = new ApplicationModule ('test-module', path.resolve (__dirname, '../fixtures/app'));
-    var routersPath = path.resolve (__dirname, '../fixtures/app/routers');
-    routerBuilder = new RouterBuilder (routersPath, appModule.controllers);
+    routersPath = path.resolve (__dirname, '../fixtures/app/routers');
+
+    routerBuilder = new RouterBuilder (appModule.controllers);
 
     it ('should create a new RouterBuilder', function () {
-      expect (routerBuilder._routerPath).to.equal (routersPath);
       expect (routerBuilder._controllers).to.equal (appModule.controllers);
-      expect (routerBuilder._currPath).to.equal ('/');
     });
   });
+  
+  describe ('#addRoutes', function () {
+    var spec = require (path.join (routersPath, 'TestRouter'));
+
+    it ('should add routes to the router', function () {
+      routerBuilder.addSpecification (spec);
+      var router = routerBuilder.getRouter ();
+
+      expect (router.params).to.have.keys (['param1', 'param2']);
+      expect (router.params.param1).to.have.length (1);
+      expect (router.params.param2).to.have.length (1);
+
+      expect (router.params.param1[0]).to.be.a.function;
+      expect (router.params.param2[0]).to.be.a.function;
+      
+      expect (router.stack[2].route.path).to.equal ('/helloworld');
+      expect (router.stack[4].route.path).to.equal ('/helloworld/inner');
+    });
+  })
 });
