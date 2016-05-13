4e2db16d8f46a3e3bee18c88a2e737c2a9a10871
James H. Hill <hilljh82@gmail.com>
2016-05-13 16:48:46 -0400

    Removed obsolete methods

--- packages/blueprint/lib/BaseController.js
+++ packages/blueprint/lib/BaseController.js
@@ -10,20 +10,6 @@ function BaseController () {
 
 }
 
-BaseController.prototype.notifyCallback = function () {
-  // Remove the first parameter from the argument list. This solution is adapted from
-  // the following StackOverflow solution:
-  //
-  //  http://stackoverflow.com/a/13296688/2245732
-  var shift = [].shift;
-  var callback = shift.apply (arguments);
-  var args = arguments;
-
-  process.nextTick (function () {
-    callback.apply (callback, args);
-  });
-};
-
 BaseController.prototype.handleError = function (err, res, status, msg, cb) {
   if (res) {
     if (msg)
@@ -34,20 +20,6 @@ BaseController.prototype.handleError = function (err, res, status, msg, cb) {
 
   if (cb)
     this.notifyCallback (err ? err : new Error (msg));
-};
-
-BaseController.prototype.uploadSingle = function (name, next) {
-  return [
-    multer.single (name),
-    next
-  ];
-};
-
-BaseController.prototype.uploadArray = function (name, maxCount, next) {
-  return [
-    multer.array (name, maxCount),
-    next
-  ];
 };
 
 module.exports = exports = BaseController;
