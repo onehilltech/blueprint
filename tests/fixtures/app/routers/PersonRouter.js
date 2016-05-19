7d25eac7e968bfa3e4d08c26cfa9b610473b14ab
James H. Hill <hilljh82@gmail.com>
2016-05-19 15:23:13 -0400

    Added blacklist and whitelist support to resources

--- tests/fixtures/app/routers/PersonRouter.js
+++ tests/fixtures/app/routers/PersonRouter.js
@@ -2,6 +2,27 @@ module.exports = exports = {
   ':personId': { property: 'personId' },
 
   '/persons': {
-    resource: {id: ':personId', controller: 'PersonController'}
+    resource: {
+      id: ':personId',
+      controller: 'PersonController'
+    }
+  },
+
+  '/allow': {
+    // only allow a subset of actions on the resource (whitelist)
+    resource: {
+      id: ':personId',
+      controller: 'PersonController',
+      allow: ['create', 'getOne']
+    }
+  },
+
+  '/deny': {
+    // prevent a subset of the actions on the resource (blacklist)
+    resource: {
+      id: ':personId',
+      controller: 'PersonController',
+      deny: ['delete']
+    }
   }
 };
