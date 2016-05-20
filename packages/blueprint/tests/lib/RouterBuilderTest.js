055b11316a132453112eb7ea28e76cc42f3db5ae
James H. Hill <hilljh82@gmail.com>
2016-05-20 01:19:10 -0400

    Fixed unit test for blacklist/whitelist methods on resources

--- packages/blueprint/tests/lib/RouterBuilderTest.js
+++ packages/blueprint/tests/lib/RouterBuilderTest.js
@@ -15,32 +15,31 @@ describe ('RouterBuilder', function () {
   var routersPath;
 
   describe ('new RouterBuilder ()', function () {
-    var appModule = new ApplicationModule ('test-module', path.resolve (__dirname, '../fixtures/app'));
-    routersPath = path.resolve (__dirname, '../fixtures/app/routers');
-
-    routerBuilder = new RouterBuilder (appModule.controllers);
-
     it ('should create a new RouterBuilder', function () {
+      var appModule = new ApplicationModule ('test-module', path.resolve (__dirname, '../fixtures/app'));
+      routersPath = path.resolve (__dirname, '../fixtures/app/routers');
+      routerBuilder = new RouterBuilder (appModule.controllers);
+
       expect (routerBuilder._controllers).to.equal (appModule.controllers);
     });
   });
   
   describe ('#addRoutes', function () {
-    var spec = require (path.join (routersPath, 'TestRouter'));
-
     it ('should add routes to the router', function () {
+      var spec = require (path.join (routersPath, 'TestRouter'));
+
       routerBuilder.addSpecification (spec);
       var router = routerBuilder.getRouter ();
 
-      expect (router.params).to.have.keys (['param1', 'param2', 'personId']);
+      expect (router.params).to.have.keys (['param1', 'param2']);
       expect (router.params.param1).to.have.length (1);
       expect (router.params.param2).to.have.length (1);
 
       expect (router.params.param1[0]).to.be.a.function;
       expect (router.params.param2[0]).to.be.a.function;
       
-      expect (router.stack[2].route.path).to.equal ('/helloworld');
-      expect (router.stack[4].route.path).to.equal ('/helloworld/inner');
+      expect (router.stack[0].route.path).to.equal ('/helloworld');
+      expect (router.stack[1].route.path).to.equal ('/helloworld/inner');
     });
   });
 
@@ -70,106 +69,108 @@ describe ('RouterBuilder', function () {
       app.database.disconnect (done);
     });
 
-    it ('should create a new resource', function (done) {
-      request (server.app)
-        .post ('/persons')
-        .send ({first_name: 'James', last_name: 'Hill'})
-        .expect (200)
-        .end (function (err, res) {
-          if (err) return done (err);
+    describe ('all', function () {
+      it ('should create a new resource', function (done) {
+        request (server.app)
+          .post ('/persons')
+          .send ({first_name: 'James', last_name: 'Hill'})
+          .expect (200)
+          .end (function (err, res) {
+            if (err) return done (err);
 
-          expect (res.body).to.have.keys (['_id']);
+            expect (res.body).to.have.keys (['_id']);
 
-          id = res.body._id;
+            id = res.body._id;
 
-          return done ();
-        });
-    });
+            return done ();
+          });
+      });
 
-    it ('should not create the resource [missing body]', function (done) {
-      request (server.app)
-        .post ('/persons')
-        .expect (400, done);
-    });
+      it ('should not create the resource [missing body]', function (done) {
+        request (server.app)
+          .post ('/persons')
+          .expect (400, done);
+      });
 
-    it ('should not create the resource [invalid parameters]', function (done) {
-      request (server.app)
-        .post ('/persons')
-        .send ({first: 'James', last: 'Hill'})
-        .expect (400, done);
-    });
+      it ('should not create the resource [invalid parameters]', function (done) {
+        request (server.app)
+          .post ('/persons')
+          .send ({first: 'James', last: 'Hill'})
+          .expect (400, done);
+      });
 
-    it ('should retrieve a single resource', function (done) {
-      request (server.app)
-        .get ('/persons/' + id)
-        .expect (200)
-        .end (function (err, res) {
-          if (err) return done (err);
+      it ('should retrieve a single resource', function (done) {
+        request (server.app)
+          .get ('/persons/' + id)
+          .expect (200)
+          .end (function (err, res) {
+            if (err) return done (err);
 
-          expect (res.body).to.have.keys (['_id', 'first_name', 'last_name']);
-          expect (res.body).to.have.property ('_id', id);
-          expect (res.body).to.have.property ('first_name', 'James');
-          expect (res.body).to.have.property ('last_name', 'Hill');
+            expect (res.body).to.have.keys (['_id', 'first_name', 'last_name']);
+            expect (res.body).to.have.property ('_id', id);
+            expect (res.body).to.have.property ('first_name', 'James');
+            expect (res.body).to.have.property ('last_name', 'Hill');
 
-          return done ();
-        });
-    });
+            return done ();
+          });
+      });
 
-    it ('should retrieve a list of all resources', function (done) {
-      request (server.app)
-        .get ('/persons')
-        .expect (200)
-        .end (function (err, res) {
-          if (err) return done (err);
+      it ('should retrieve a list of all resources', function (done) {
+        request (server.app)
+          .get ('/persons')
+          .expect (200)
+          .end (function (err, res) {
+            if (err) return done (err);
 
-          expect (res.body).to.have.length (1);
-          expect (res.body[0]._id).to.equal (id);
+            expect (res.body).to.have.length (1);
+            expect (res.body[0]._id).to.equal (id);
 
-          return done ();
-        });
-    });
+            return done ();
+          });
+      });
 
-    it ('should update a single resource', function (done) {
-      async.series ([
-        function (callback) {
-          request (server.app)
-            .put ('/persons/' + id)
-            .send ({first_name: 'Lanita', last_name: 'Hill'})
-            .expect (200, 'true', callback);
-        },
+      it ('should update a single resource', function (done) {
+        async.series ([
+          function (callback) {
+            request (server.app)
+              .put ('/persons/' + id)
+              .send ({first_name: 'Lanita', last_name: 'Hill'})
+              .expect (200, 'true', callback);
+          },
 
-        function (callback) {
-          request (server.app)
-            .get ('/persons/' + id)
-            .expect (200)
-            .end (function (err, res) {
-              if (err) return callback (err);
+          function (callback) {
+            request (server.app)
+              .get ('/persons/' + id)
+              .expect (200)
+              .end (function (err, res) {
+                if (err) return callback (err);
 
-              expect (res.body).to.have.keys (['_id', 'first_name', 'last_name']);
-              expect (res.body).to.have.property ('first_name', 'Lanita');
-              expect (res.body).to.have.property ('last_name', 'Hill');
+                expect (res.body).to.have.keys (['_id', 'first_name', 'last_name']);
+                expect (res.body).to.have.property ('first_name', 'Lanita');
+                expect (res.body).to.have.property ('last_name', 'Hill');
 
-              return callback ();
-            });
-        }
+                return callback ();
+              });
+          }
 
-      ], done);
-    });
+        ], done);
+      });
 
-    it ('should delete an existing resource', function (done) {
-      request (server.app)
-        .delete ('/persons/' + id)
-        .expect (200, 'true', done);
-    });
+      it ('should delete an existing resource', function (done) {
+        request (server.app)
+          .delete ('/persons/' + id)
+          .expect (200, 'true', done);
+      });
 
-    it ('should not delete an existing resource', function (done) {
-      request (server.app)
-        .delete ('/persons/' + id)
-        .expect (404, done);
+      it ('should not delete an existing resource', function (done) {
+        request (server.app)
+          .delete ('/persons/' + id)
+          .expect (404, done);
+      });
     });
 
     // allowed routing...
-    describe ('property: allow', function () {
+    describe ('whitelist', function () {
 
       // allow: create, getOne
 
@@ -195,12 +196,12 @@ describe ('RouterBuilder', function () {
 
       it ('should not retrieve all the resources', function (done) {
         request (server.app)
-          .delete ('/allow')
+          .get ('/allow')
           .expect (404, done);
       });
     });
 
-    describe ('property: deny', function () {
+    describe ('blacklist', function () {
 
       // deny: delete
 
