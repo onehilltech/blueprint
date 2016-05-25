c75e4a98a700f287ecb1c1efa8ca72ca146eab56
James H. Hill <hilljh82@gmail.com>
2016-05-25 00:15:18 -0400

    Cleaned up account router

--- packages/blueprint-gatekeeper/app/routers/v1/AccountsRouter.js
+++ packages/blueprint-gatekeeper/app/routers/v1/AccountsRouter.js
@@ -13,49 +13,4 @@ module.exports = exports = {
     use: passport.authenticate ('bearer', {session: false}),
     resource: { id: ':accountId', controller: 'AccountController' }
   }
-
-  /*
-  // Define the different account routes. We are going to protect all routes
-  // under the /accounts base uri.
-  '/accounts' : {
-
-    post: {
-      before : [
-        gatekeeper.authorization.isClient (),
-        gatekeeper.authorization.roles.any ([gatekeeper.roles.client.account.create])
-      ],
-      action : 'AccountController@createAccount'
-    },
-
-    '/:accountId' : {
-      // Only an administrator can access the account information. The check
-      // below applies to all paths that begin with this prefix.
-
-      use : [
-        gatekeeper.authorization.roles.any ([gatekeeper.roles.user.administrator])
-      ],
-
-      get : { action : 'AccountController@getAccount'},
-      
-      delete: {
-        before : [
-          gatekeeper.authorization.isClient (),
-          gatekeeper.authorization.roles.any ([gatekeeper.roles.client.account.delete])
-        ],
-        action : 'AccountController@deleteAccount'
-      },
-
-      '/profile' : {
-        get : { action : 'AccountController@getProfile' }
-      },
-
-      '/enable' : {
-        post : { action : 'AccountController@enableAccount' }
-      },
-
-      '/roles' : {
-        post : { action : 'AccountController@updateRoles' }
-      }
-    }
-  }*/
 };
