3d03cef9411d6e8c11761ed01c130ece49aa1761
James H. Hill <hilljh82@gmail.com>
2016-06-23 20:11:50 -0400

    Updated to latest version of Blueprint

--- packages/blueprint-gatekeeper/app/controllers/oauth2/WorkflowController.js
+++ packages/blueprint-gatekeeper/app/controllers/oauth2/WorkflowController.js
@@ -9,6 +9,9 @@ var Client      = require ('../../models/Client')
   , AccessToken = require ('../../models/oauth2/AccessToken')
   ;
 
+var HttpError = blueprint.errors.HttpError
+  ;
+
 /**
  * @class WorkflowController
  *
@@ -63,21 +66,21 @@ WorkflowController.prototype.issueToken = function (callback) {
     });
   }
 
-  function lookupClient (res, clientId, clientSecret, done) {
+  function lookupClient (clientId, clientSecret, callback) {
     Client.findById (clientId, function (err, client) {
       if (err)
-        return self.handleError (err, res, 400, 'Failed to lookup client', callback);
+        return callback (new HttpError (400, 'Failed to lookup client'));
 
       if (!client)
-        return self.handleError (null, res, 400, 'Invalid client id', callback);
+        return callback (new HttpError (400, 'Invalid client id'));
 
       if (!client.enabled)
-        return self.handleError (null, res, 401, 'Client is not enabled', callback);
-
+        return callback (new HttpError (401, 'Client is not enabled'));
+      
       if (clientSecret && client.secret !== clientSecret)
-        return self.handleError (null, res, 400, 'Client secret is incorrect', callback);
+        return callback (new HttpError (400, 'Client secret is incorrect'));
 
-      done (client);
+      callback (null, client);
     });
   }
 
@@ -86,28 +89,28 @@ WorkflowController.prototype.issueToken = function (callback) {
    *
    * @param req
    * @param res
+   * @param callback
    */
-  function client_credentials (req, res) {
+  function client_credentials (req, res, callback) {
     req.checkBody ('client_id', 'required').notEmpty();
     req.checkBody ('client_secret', 'required').notEmpty();
 
     var errs = req.validationErrors (true);
-
-    if (errs)
-      return self.handleError (null, res, 400, errs, callback);
+    if (errs) return callback (new HttpError (400, errs));
 
     var clientId = req.body.client_id;
     var clientSecret = req.body.client_secret;
 
-    lookupClient (res, clientId, clientSecret, function (client) {
+    lookupClient (clientId, clientSecret, function (err, client) {
+      if (err) return callback (err);
+
       // Authenticate the username/password combo. Upon authentication, we
       // are to return the token/refresh_token combo.
       winston.log ('info', 'client %s: exchanging secret for access token', client.id);
 
       // Create a new user token and refresh token.
       AccessToken.createClientToken (client.id, '*', function (err, accessToken) {
-        if (err)
-          return self.handleError (err, res, 500, 'Failed to generate token', callback);
+        if (err) return callback (new HttpError (500, 'Failed to generate token'));
 
         return grantToken (res, accessToken);
       });
@@ -120,22 +123,22 @@ WorkflowController.prototype.issueToken = function (callback) {
    * @param req
    * @param res
    */
-  function password (req, res) {
+  function password (req, res, callback) {
     req.checkBody ('client_id', 'required').notEmpty();
     req.checkBody ('username', 'required').notEmpty();
     req.checkBody ('password', 'required').notEmpty();
 
     var errs = req.validationErrors (true);
-
-    if (errs)
-      return self.handleError (null, res, 400, errs, callback);
+    if (errs) return callback (new HttpError (400, errs));
 
     var clientId = req.body.client_id;
     var username = req.body.username;
     var password = req.body.password;
 
     // Locate the client and make sure the client is enabled.
-    lookupClient (res, clientId, null, function (client) {
+    lookupClient (clientId, null, function (err, client) {
+      if (err) return callback (err);
+
       // Authenticate the username/password combo. Upon authentication, we
       // are to return the token/refresh_token combo.
       winston.log ('info', 'client %s: exchanging username/password for access token [user=%s]', client.id, username);
@@ -144,27 +147,27 @@ WorkflowController.prototype.issueToken = function (callback) {
         // Check the result of the operation. If the account does not exist or the
         // account is disabled, then return the appropriate error message.
         if (err)
-          return self.handleError (err, res, 500, 'Failed to retrieve account', callback);
+          return callback (new HttpError (500, 'Failed to retrieve account'));
 
         if (!account)
-          return self.handleError (err, res, 400, 'Invalid username', callback);
+          return callback (new HttpError (400, 'Invalid username'));
 
         if (!account.isEnabled ())
-          return self.handleError (err, res, 401, 'Account is disabled', callback);
+          return callback (new HttpError (401, 'Account is disabled'));
 
         account.verifyPassword (password, function (err, match) {
           // Check the result of the operation. If there is an error, or the password
           // does not match, then return an error.
           if (err)
-            return self.handleError (err, res, 500, 'Failed to verify password', callback);
+            return callback (new HttpError (500, 'Failed to verify password'));
 
           if (!match)
-            return self.handleError (err, res, 401, 'Invalid password', callback);
+            return callback (new HttpError (401, 'Invalid password'));
 
           // Create a new user token and refresh token.
           AccessToken.createUserToken (client.id, account.id, function (err, accessToken) {
             if (err)
-              return self.handleError (err, res, 500, 'Failed to generate access token', callback);
+              return callback (new HttpError (500, 'Failed to generate access token'));
 
             return grantToken (res, accessToken);
           });
@@ -179,14 +182,12 @@ WorkflowController.prototype.issueToken = function (callback) {
    * @param req
    * @param res
    */
-  function refresh_token (req, res) {
+  function refresh_token (req, res, callback) {
     req.checkBody ('client_id', 'required').notEmpty ();
     req.checkBody ('refresh_token', 'required').notEmpty ();
 
     var errs = req.validationErrors (true);
-
-    if (errs)
-      return self.handleError (null, res, 400, errs, callback);
+    if (errs) return callback (new HttpError (400, errs));
 
     var clientId = req.body.client_id;
     var clientSecret = req.body.client_secret;
@@ -197,38 +198,42 @@ WorkflowController.prototype.issueToken = function (callback) {
       .populate ('account client')
       .exec (function (err, at) {
         if (err)
-          return self.handleError (err, res, 500, 'Failed to locate refresh token', callback);
+          return callback (new HttpError (500, 'Failed to locate refresh token'));
 
         if (!at)
-          return self.handleError (err, res, 400, 'Refresh token is invalid', callback);
+          return callback (new HttpError (400, 'Refresh token is invalid'));
 
         // Check the client and account. The client and the account must be enabled. The
         // client secret must also match, if provided.
         if (!at.client.enabled)
-          return self.handleError (err, res, 401, 'Client access is disabled', callback);
+          return callback (new HttpError (401, 'Client access is disabled'));
 
         if (clientSecret && at.client.secret !== clientSecret)
-          return self.handleError (err, res, 400, 'Client secret is incorrect', callback);
+          return callback (new HttpError (400, 'Client secret is incorrect'));
 
         if (!at.account.isEnabled ())
-          return self.handleError (err, res, 400, 'User account is disabled', callback);
+          return callback (new HttpError (400, 'User account is disabled'));
 
         // Generate a new access and refresh token.
         async.waterfall ([
           function (callback) {
             AccessToken.generateTokenString (callback);
           },
+
           function (token, callback) {
-            at.token = token;
-            AccessToken.generateTokenString (callback);
+            AccessToken.generateTokenString (function (err, token) {
+              return callback (null, token, refreshToken);
+            });
           },
-          function (token, callback) {
+
+          function (token, refreshToken, callback) {
+            at.token = token;
             at.refresh_token = token;
+
             at.save (callback);
           }
         ], function (err, at) {
-          if (err)
-            return self.handleError (err, res, 500, 'Failed to save new token', callback);
+          if (err) return callback (new HttpError (500, 'Failed to save new token'));
 
           grantToken (res, at);
         });
@@ -247,14 +252,14 @@ WorkflowController.prototype.issueToken = function (callback) {
       return callback (req.validationErrors (true));
     },
 
-    execute: function (req, res) {
+    execute: function (req, res, callback) {
 
       // Locate the handler for the grant type.
       var grantType = req.body.grant_type;
       var grantFunc = grantTypes[grantType];
 
       // Handle the token request.
-      return grantFunc (req, res);
+      return grantFunc (req, res, callback);
     }
   };
 };
