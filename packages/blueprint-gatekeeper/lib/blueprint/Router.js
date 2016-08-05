b28f443726fb81a5c3e74da90d2bb9ac4403d3f6
James H. Hill <hilljh82@gmail.com>
2016-08-04 20:18:43 -0400

    Pre-built router failed to load

--- packages/blueprint-gatekeeper/lib/blueprint/Router.js
+++ packages/blueprint-gatekeeper/lib/blueprint/Router.js
@@ -1,15 +1,57 @@
 var blueprint = require ('@onehilltech/blueprint')
+  , messaging = blueprint.messaging
   , cors      = require ('cors')
   , winston   = require ('winston')
   , path      = require ('path')
   , express   = require ('express')
+  , util      = require ('util')
   ;
 
 const DEFAULT_VERSION = 1;
 
-var router = express.Router ();
+function makeRouter (moduleName, version) {
+  var router = express.Router ();
+  version = 'v' + (version || DEFAULT_VERSION);
 
-module.exports = exports = router;
+  var app = blueprint.app;
+
+  function initRouter (app) {
+    var corsConfig = app.configs.cors || {};
+    var options = corsConfig.options || {};
+
+    if (!options.origin)
+      options.origin = origin;
+
+    // Locate the Gatekeeper module in the application.
+    var module = app.modules[moduleName];
+
+    if (!module)
+      throw Error (util.format ('Module [%s] does not exist', moduleName));
+
+    // Select the router version.
+    var routers = module.routers[version];
+    var routerBuilder = new blueprint.RouterBuilder ('/');
+    routerBuilder.addRouters (routers);
+
+    var handlers = [cors (options)].concat (routerBuilder.getRouter ());
+    router.use (handlers);
+
+    messaging.emit ('gatekeeper.router.init', router);
+  }
+
+  if (app.is_init) {
+    process.nextTick (function () {
+      initRouter (app);
+    })
+  }
+  else {
+    messaging.once ('app.init', function (app) { initRouter (app); });
+  }
+
+  return router;
+}
+
+module.exports = exports = makeRouter;
 
 /**
  * Check if the origin is whitelisted. By default, all origins are whitelisted
@@ -25,28 +67,3 @@ function origin (origin, callback) {
   callback (null, true);
 }
 
-var modulePath = path.resolve (__dirname, '../../app');
-var appModule = new blueprint.ApplicationModule (modulePath);
-
-// Initialize the application module.
-
-appModule.init (function (err, appModule) {
-  if (err) throw err;
-
-  var gatekeeperConfig = blueprint.app.configs.gatekeeper || {};
-  var corsConfig = blueprint.app.configs.cors || {};
-  var options = corsConfig.options || {};
-
-  if (!options.origin)
-    options.origin = origin;
-
-  // Select the router version.
-  var version = 'v' + (gatekeeperConfig.version || DEFAULT_VERSION);
-  var routerSpec = appModule.routers[version];
-
-  var routerBuilder = new blueprint.RouterBuilder ('/');
-  routerBuilder.addRouters (routerSpec);
-
-  var handlers = [cors (options)].concat (routerBuilder.getRouter ());
-  router.use (handlers);
-});
