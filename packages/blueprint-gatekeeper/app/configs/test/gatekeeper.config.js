e80ccafc3ed96e3d4f54890a8e9f9236bc033d3f
James H. Hill <hilljh82@gmail.com>
2016-08-05 00:06:09 -0400

    Added configuration parameters for issuer, audience, and subject in jwt

--- packages/blueprint-gatekeeper/app/configs/test/gatekeeper.config.js
+++ packages/blueprint-gatekeeper/app/configs/test/gatekeeper.config.js
@@ -1,13 +1,19 @@
+var fs = require ('fs')
+  , path = require ('path')
+  ;
 
 module.exports = exports = {
   activation: {
-    required: true,
+    required: true
   },
 
   token: {
     kind: 'jwt',
     options: {
-      secret: 'shhhhh'
+      issuer: 'gatekeeper',
+      algorithm: 'RS256',
+      publicKey: fs.readFileSync (path.resolve (__dirname, '../../resources/ssl/server.crt')),
+      privateKey: fs.readFileSync (path.resolve (__dirname, '../../resources/ssl/server.key'))
     }
   }
 };
--- packages/blueprint-gatekeeper/lib/tokens/JwtToken.js
+++ packages/blueprint-gatekeeper/lib/tokens/JwtToken.js
@@ -1,6 +1,8 @@
 var jwt = require ('jsonwebtoken')
   ;
 
+const DEFAULT_ALGORITHM = 'HS256';
+
 function JwtToken (opts) {
   this._opts = opts || {};
 
@@ -13,11 +15,37 @@ JwtToken.prototype.generateToken = function (opts, callback) {
   var options = opts.options;
 
   var hash = this._opts.secret || this._opts.privateKey;
+
+  if (this._opts.privateKey)
+    options.algorithm = this._opts.algorithm || DEFAULT_ALGORITHM;
+
+  if (this._opts.issuer)
+    options.issuer = options.issuer || this._opts.issuer;
+
+  if (this._opts.subject)
+    options.subject = options.subject || this._opts.subject;
+
+  if (this._opts.audience)
+    options.audience = options.audience || this._opts.audience;
+
   return jwt.sign (payload, hash, options, callback);
 };
 
 JwtToken.prototype.verifyToken = function (token, opts, callback) {
   var hash = this._opts.secret || this._opts.publicKey;
+
+  if (this._opts.publicKey)
+    opts.algorithms = [this._opts.algorithm || DEFAULT_ALGORITHM];
+
+  if (this._opts.issuer)
+    opts.issuer = opts.issuer || this._opts.issuer;
+
+  if (this._opts.subject)
+    opts.subject = opts.subject || this._opts.subject;
+
+  if (this._opts.audience)
+    opts.audience = opts.audience || this._opts.audience;
+
   return jwt.verify (token, hash, opts, callback);
 };
 
