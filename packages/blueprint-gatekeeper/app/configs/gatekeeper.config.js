79d595c3e727ff16c54047c10cb22b10394fa044
James H. Hill <hilljh82@gmail.com>
2016-08-09 22:37:32 -0400

    Updated to latest version of Blueprint

--- packages/blueprint-gatekeeper/app/configs/gatekeeper.config.js
+++ packages/blueprint-gatekeeper/app/configs/gatekeeper.config.js
@@ -1,3 +1,6 @@
+var blueprint = require ('@onehilltech/blueprint')
+  ;
+
 module.exports = exports = {
   baseuri : 'http://localhost:5000/gatekeeper',
 
@@ -7,6 +10,16 @@ module.exports = exports = {
 
     nodemailer : {
       transport: 'stub'
+    }
+  },
+
+  token: {
+    kind: 'jwt',
+    options: {
+      algorithm: 'RS256',
+      publicKey:  blueprint.app.resource ('ssl/server.crt'),
+      privateKey: blueprint.app.resource ('ssl/server.key'),
+      issuer: 'engage'
     }
   }
 };
