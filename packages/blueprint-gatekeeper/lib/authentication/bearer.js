0d40b2b3a09796289e6612cf9ff9148c1e8eb30c
James H. Hill <hilljh82@gmail.com>
2016-08-11 23:11:03 -0400

    Fixed error reporting in bearer strategy

--- packages/blueprint-gatekeeper/lib/authentication/bearer.js
+++ packages/blueprint-gatekeeper/lib/authentication/bearer.js
@@ -38,58 +38,59 @@ module.exports = exports = function (opts) {
 
   return new BearerStrategy (function (token, done) {
     async.waterfall ([
+      // Verify the token.
       function (callback) {
-        // First, verify the token. There is no need to continue if we cannot
-        // verify the authenticity of the token.
         tokenStrategy.verifyToken (token, {}, callback);
       },
 
+      // Locate details about the token in the database.
       function (payload, callback) {
-        async.waterfall ([
-          // Locate the access token in the database.
-          function (callback) {
-            AccessToken
-              .findById (payload.jti)
-              .populate ('client', clientPopulation)
-              .populate ('account', accountPopulation)
-              .exec (callback);
-          },
+        AccessToken
+          .findById (payload.jti)
+          .populate ('client', clientPopulation)
+          .populate ('account', accountPopulation)
+          .exec (function (err, accessToken) {
+            return callback (err, payload, accessToken);
+          });
+      }
+    ], function (err, payload, accessToken) {
+      if (err) {
+        // Process the error message. We have to check the name because the error
+        // could be related to token verification.
+        if (err.name === 'TokenExpiredError')
+          return done (null, false, {message: 'Token has expired'});
 
-          // Validate if the token can access the resources.
-          function (accessToken, callback) {
-            if (!accessToken)
-              return callback (new Error ('Unknown access token'));
+        if (err.name === 'JsonWebTokenError')
+          return done (null, false, {message: err.message});
 
-            if (!accessToken.enabled)
-              return callback (new Error ('Access token is disabled'));
-
-            if (!accessToken.client)
-              return callback (new Error ('Client is unknown'));
-
-            if (!accessToken.client.enabled)
-              return callback (new Error ('Token is from an unknown client'));
-
-            // Set the user to the client id.
-            var user = accessToken.client;
-
-            if (payload.kind === 'user') {
-              if (!accessToken.account)
-                return callback (new Error ('Unknown account'));
-
-              if (!accessToken.account.enabled)
-                return callback (new Error ('Account is disabled'));
-
-              // Update the user to the account id.
-              user = accessToken.account;
-            }
-
-            return callback (null, user, payload, accessToken);
-          }
-        ], callback);
+        return done (err);
       }
 
-    ], function (err, user, payload, accessToken) {
-      if (err) return done (err);
+      if (!accessToken)
+        return done (null, false, {message: 'Unknown access token'});
+
+      if (!accessToken.enabled)
+        return done (null, false, {message: 'Token is disabled'});
+
+      if (!accessToken.client)
+        return done (null, false, {message: 'Unknown client'});
+
+      if (!accessToken.client.enabled)
+        return done (null, false, {message: 'Client is disabled'});
+
+      // Set the user to the client id.
+      var user = accessToken.client;
+
+      if (payload.kind === 'user') {
+        if (!accessToken.account)
+          return done (null, false, {message: 'Unknown account'});
+
+        if (!accessToken.account.enabled)
+          return done (null, false, {message: 'Account is disabled'});
+
+        // Update the user to the account id.
+        user = accessToken.account;
+      }
 
       var authInfo = {scope: payload.kind, token: accessToken};
       return done (null, user, authInfo);
