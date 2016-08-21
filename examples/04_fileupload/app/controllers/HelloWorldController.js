af3b6781a4603452a8ed53673e1611b4d6240605
James H. Hill <hilljh82@gmail.com>
2016-08-20 23:47:36 -0400

    Re-implement example

--- examples/04_fileupload/app/controllers/HelloWorldController.js
+++ examples/04_fileupload/app/controllers/HelloWorldController.js
@@ -12,17 +12,8 @@ blueprint.controller (HelloWorldController);
 
 HelloWorldController.prototype.uploadImage = function () {
   return blueprint.app.server.upload.singleFile ('image', function (req, res) {
-    blueprint.app.database.gridfs.writeFileToDatabase (req.file, function (file) {
-      return res.render ('helloworld', {file : file});
-    });
+    return res.render ('helloworld.pug', {file: req.file});
   });
-};
-
-HelloWorldController.prototype.getImage = function () {
-  return function (req, res) {
-    var imageId = req.imageId;
-    blueprint.app.database.gridfs.createReadStream ({_id : imageId}).pipe (res);
-  };
 };
 
 module.exports = exports = HelloWorldController;
--- examples/04_fileupload/app/routers/MainRouter.js
+++ examples/04_fileupload/app/routers/MainRouter.js
@@ -1,12 +1,6 @@
 module.exports = exports = {
-  ':imageId' : { property : 'imageId'},
-
   '/helloworld' : {
-    get  : { view   : 'helloworld' },
-    post : { action : 'HelloWorldController@uploadImage'},
-  },
-
-  '/helloworld/images/:imageId' : {
-    get : { action : 'HelloWorldController@getImage' }
+    get: {view: 'helloworld.pug'},
+    post: {action: 'HelloWorldController@uploadImage'}
   }
 };
