bf36ea1c75488b11c0be50592eb88507cf20beae
James H. Hill <hilljh82@gmail.com>
2016-08-26 00:48:57 -0400

    Fixed build errors

--- packages/blueprint-gatekeeper/lib/index.js
+++ packages/blueprint-gatekeeper/lib/index.js
@@ -1,6 +1,5 @@
 exports.auth = require ('./authentication');
 exports.authentication = exports.auth;
-exports.blueprint = require ('./blueprint');
 exports.newClient = require ('./GatekeeperClient');
 exports.roles = require ('./roles');
 exports.tokens = require ('./tokens');
