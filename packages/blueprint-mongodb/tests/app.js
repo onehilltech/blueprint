f7463a147696385a91f9580b07c61a78ec812165
James H. Hill <hilljh82@gmail.com>
2016-10-01 20:33:06 -0400

    Removed unnecessary files

--- packages/blueprint-mongodb/tests/app.js
+++
@@ -1,9 +0,0 @@
-var blueprint = require ('@onehilltech/blueprint')
-  , path = require ('path')
-  ;
-
-var appPath = path.resolve (__dirname, 'fixtures/app');
-
-module.exports = function (callback) {
-  blueprint.Application (appPath, callback);
-};
