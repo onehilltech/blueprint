0cba6c24af3ad7bb98185795ea6c6f1d09fa3d78
James H. Hill <hilljh82@gmail.com>
2016-10-03 02:04:32 -0400

    Updated dependencies

--- packages/blueprint-gatekeeper/lib/index.js
+++ packages/blueprint-gatekeeper/lib/index.js
@@ -1,5 +1,10 @@
+'use strict';
+
+var exports = module.exports;
+
 exports.auth = require ('./authentication');
 exports.authentication = exports.auth;
+
 exports.newClient = require ('./GatekeeperClient');
 exports.roles = require ('./roles');
 exports.tokens = require ('./tokens');
