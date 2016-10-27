a81145fd9ef46e82003aee8f43888c130e40f0b1
James H. Hill <hilljh82@gmail.com>
2016-10-26 21:30:46 -0400

    Added helper method for checking schema, then performing other validation

--- packages/blueprint/lib/BaseController.js
+++ packages/blueprint/lib/BaseController.js
@@ -10,22 +10,23 @@ function BaseController () {
 
 }
 
-BaseController.prototype.handleError = function (err, res, status, msg, cb) {
-  if (res) {
-    if (msg)
-      res.status (status).send (msg);
-    else
-      res.sendStatus (status);
-  }
-
-  if (cb)
-    this.notifyCallback (err ? err : new Error (msg));
-};
-
 BaseController.prototype.notFound = function () {
   return function (req, res) {
     res.status (404).json ({error: 'Not Found'});
   };
 };
 
-module.exports = exports = BaseController;
+BaseController.prototype.checkSchemaThen = function (schema, then) {
+  return function (req, callback) {
+    async.series ([
+      function (callback) {
+        req.check (schema);
+        return callback (req.validationErrors ());
+      },
+
+      then
+    ], callback);
+  };
+};
+
+  module.exports = exports = BaseController;
