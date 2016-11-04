b204e28f6dfb6da39d32626a5cdeda082c68d245
James H. Hill <hilljh82@gmail.com>
2016-11-04 12:32:49 -0400

    Removed client routes

--- packages/blueprint-gatekeeper/app/routers/v1/ClientRouter.js
+++
@@ -1,9 +0,0 @@
-var passport   = require ('passport')
-  ;
-
-module.exports = exports = {
-  '/clients' : {
-    use: passport.authenticate ('bearer', {session: false}),
-    resource: { controller: 'ClientController' }
-  }
-};
