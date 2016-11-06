e9cb19461bba0533b3295c776b6f1e69ddf4bda9
James H. Hill <hilljh82@gmail.com>
2016-11-06 00:46:25 -0400

    Updated default roles for setup clients

--- packages/blueprint-gatekeeper/lib/roles/client.js
+++ packages/blueprint-gatekeeper/lib/roles/client.js
@@ -2,7 +2,15 @@ var roles = {
   administrator: 'gatekeeper.administrator',
 
   account: {
-    create: 'gatekeeper.account.create'
+    create: 'gatekeeper.account.create',
+    update: 'gatekeeper.account.update',
+    delete: 'gatekeeper.account.delete'
+  },
+
+  client: {
+    create: 'gatekeeper.client.create',
+    update: 'gatekeeper.client.update',
+    delete: 'gatekeeper.client.delete'
   }
 };
 
