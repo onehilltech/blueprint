bdc0f47d5c983388e84cf2aa35bcb02117e45117
James H. Hill <hilljh82@gmail.com>
2016-11-24 13:06:11 -0600

    Simplified protecting routes

--- packages/blueprint-gatekeeper/lib/index.js
+++ packages/blueprint-gatekeeper/lib/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var passport = require ('passport')
+  ;
+
 var exports = module.exports;
 
 exports.auth = require ('./authentication');
@@ -12,3 +15,9 @@ exports.tokens = require ('./tokens');
 exports.__defineGetter__ ('testing', function () {
   return require ('./testing');
 });
+
+function protect () {
+  return passport.authenticate ('bearer', {session: false});
+}
+
+exports.protect = protect;
