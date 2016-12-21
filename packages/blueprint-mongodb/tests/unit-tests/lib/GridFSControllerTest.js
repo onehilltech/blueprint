763f576dd0c75ea6da8f8986a053217daf52ad2e
James H. Hill <hilljh82@gmail.com>
2016-12-21 01:50:52 -0500

    Refactored test

--- packages/blueprint-mongodb/tests/unit-tests/lib/GridFSControllerTest.js
+++ packages/blueprint-mongodb/tests/unit-tests/lib/GridFSControllerTest.js
@@ -30,72 +30,76 @@ describe ('GridFSController', function () {
     ], done);
   });
 
-  describe ('POST (i.e., create)', function () {
-    it ('should upload file, and store in database', function (done) {
-      var imageFile = path.resolve (__dirname, '../../data/avatar1.png');
+  describe ('/images', function () {
+    describe ('POST', function () {
+      it ('should upload file, and store in database', function (done) {
+        var imageFile = path.resolve (__dirname, '../../data/avatar1.png');
 
-      request (server.app)
-        .post ('/images')
-        .attach ('image', imageFile)
-        .expect (200, function (err, res) {
-          if (err) return done (err);
+        request (server.app)
+          .post ('/images')
+          .attach ('image', imageFile)
+          .expect (200, function (err, res) {
+            if (err) return done (err);
 
-          expect (res.body).to.have.keys (['_id']);
-          imageId = res.body._id;
+            expect (res.body).to.have.keys (['_id']);
+            imageId = res.body._id;
 
-          return done ();
-        });
+            return done ();
+          });
+      });
     });
   });
 
-  describe ('GET', function () {
-    it ('should get the image from the database', function (done) {
-      request (server.app)
-        .get ('/images/' + imageId)
-        .expect (200, function (err, res) {
-          if (err) return done (err);
+  describe ('/images/:imageId', function () {
+    describe ('GET', function () {
+      it ('should get the image from the database', function (done) {
+        request (server.app)
+          .get ('/images/' + imageId)
+          .expect (200, function (err, res) {
+            if (err) return done (err);
 
-          expect (res.type).to.equal ('image/png');
+            expect (res.type).to.equal ('image/png');
 
-          return done ();
-        });
+            return done ();
+          });
+      });
+
+      it ('should not find the image', function (done) {
+        request (server.app)
+          .get ('/images/5')
+          .expect (404, done);
+      });
+
+      it ('does not support querying for resource', function (done) {
+        request (server.app)
+          .get ('/images?filename=avatar1.png')
+          .expect (404, done);
+      })
     });
 
-    it ('should not find the image', function (done) {
-      request (server.app)
-        .get ('/images/5')
-        .expect (404, done);
+    describe ('PUT', function () {
+      it ('should not update the image', function (done) {
+        var imageFile = path.resolve (__dirname, '../../data/avatar2.png');
+
+        request (server.app)
+          .put ('/images/' + imageId)
+          .attach ('image', imageFile)
+          .expect (404, done);
+      });
     });
 
-    it ('does not support querying for resource', function (done) {
-      request (server.app)
-        .get ('/images?filename=avatar1.png')
-        .expect (404, done);
-    })
-  });
+    describe ('DELETE', function () {
+      it ('should delete the image from the database', function (done) {
+        request (server.app)
+          .delete ('/images/' + imageId)
+          .expect (200, 'true', done);
+      });
 
-  describe ('PUT (i.e., update)', function () {
-    it ('should not update the image', function (done) {
-      var imageFile = path.resolve (__dirname, '../../data/avatar2.png');
-
-      request (server.app)
-        .put ('/images/' + imageId)
-        .attach ('image', imageFile)
-        .expect (404, done);
-    });
-  });
-
-  describe ('DELETE', function () {
-    it ('should delete the image from the database', function (done) {
-      request (server.app)
-        .delete ('/images/' + imageId)
-        .expect (200, 'true', done);
-    });
-
-    it ('should not delete the image again', function (done) {
-      request (server.app)
-        .delete ('/images/' + imageId)
-        .expect (500, done);
+      it ('should not delete the image again', function (done) {
+        request (server.app)
+          .delete ('/images/' + imageId)
+          .expect (500, done);
+      });
     });
   });
 });
