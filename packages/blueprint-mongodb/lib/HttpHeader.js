e17362dfdcff325cd8e0d1b9e7440387c59a7d12
James H. Hill <hilljh82@gmail.com>
2016-12-21 19:25:50 -0500

    Added constants for If-Modified-Since and If-Unmodified-Since

--- packages/blueprint-mongodb/lib/HttpHeader.js
+++ packages/blueprint-mongodb/lib/HttpHeader.js
@@ -3,3 +3,5 @@
 var exports = module.exports = {};
 
 exports.LAST_MODIFIED = 'Last-Modified';
+exports.IF_MODIFIED_SINCE = 'If-Modified-Since';
+exports.IF_UNMODIFIED_SINCE = 'If-Unmodified-Since';
