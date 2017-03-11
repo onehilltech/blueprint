83fee2f9d3f95affa382b00d2a78f4ad3eb2f81e
James H. Hill <hilljh82@gmail.com>
2017-03-11 02:09:23 -0500

    Renamed policies

--- packages/blueprint-gatekeeper/app/policies/gatekeeper/is_client_owner.js
+++
@@ -1,8 +0,0 @@
-/**
- * Policy Description:
- *
- * This policy determine if the request is from a client.
- */
-module.exports = exports = function (req, callback) {
-  return callback (null, req.params.clientId === req.user.id);
-};
