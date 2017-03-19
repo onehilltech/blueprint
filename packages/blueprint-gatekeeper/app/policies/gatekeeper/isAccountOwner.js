ed2e94e7932412eae1c8c16fa7081072c8c8fea2
James H. Hill <hilljh82@gmail.com>
2017-03-19 13:48:27 -0400

    Replaced isAccountOwner policiy with gatekeeper.account.isMe

--- packages/blueprint-gatekeeper/app/policies/gatekeeper/isAccountOwner.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function isAccountOwner (req, callback) {
-  var accountId = req.params.accountId;
-  var result = accountId.equals (req.user._id);
-
-  return callback (null, result, 'Not the account owner');
-};
