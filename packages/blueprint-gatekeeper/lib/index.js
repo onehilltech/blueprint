41bac6050911cce86207664b1bcaefaf38588bbf
James H. Hill <hilljh82@gmail.com>
2017-04-13 15:41:09 -0400

    Remove authentication module in favor of policies

--- packages/blueprint-gatekeeper/lib/index.js
+++ packages/blueprint-gatekeeper/lib/index.js
@@ -2,9 +2,6 @@
 
 var exports = module.exports;
 
-exports.auth = require ('./authentication');
-exports.authentication = exports.auth;
-
 exports.scope = require ('./scope');
 exports.tokens = require ('./tokens');
 exports.middleware = require ('../app/middleware');
