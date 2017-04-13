c7bff3024353413b18629be097a6b0acd77b7274
James H. Hill <hilljh82@gmail.com>
2017-04-13 15:39:46 -0400

    Update to latest version of Blueprint

--- packages/blueprint-gatekeeper/app/index.js
+++ packages/blueprint-gatekeeper/app/index.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 
-var winston   = require ('winston')
-  , blueprint = require ('@onehilltech/blueprint')
+var blueprint = require ('@onehilltech/blueprint')
   ;
 
-blueprint.Application (__dirname, function (err, app) {
-  if (err) throw err;
-
-  app.start (function (err) {
-    if (err) throw err;
-
-    winston.log ('info', 'application started...');
-  });
+blueprint.createApplicationAndStart (__dirname, function (err) {
+  if (err)
+    throw err;
 });
