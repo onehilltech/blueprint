c88108db228dc523912775cbc6b19b1380879148
James H. Hill <hilljh82@gmail.com>
2017-04-14 19:00:02 -0400

    Optional gatekeeper.account.password policy to enforcing password rules/policies

--- packages/blueprint-gatekeeper/app/policies/gatekeeper/account/create.js
+++ packages/blueprint-gatekeeper/app/policies/gatekeeper/account/create.js
@@ -5,7 +5,21 @@ var blueprint  = require ('@onehilltech/blueprint')
   , gatekeeper = require ('../../../../lib')
   ;
 
-module.exports = Policy.any ([
-  Policy.assert ('gatekeeper.request.hasScope', gatekeeper.scope.account.create),
-  Policy.assert ('gatekeeper.isSuperUser')
+module.exports = Policy.all ([
+  /*
+   * Check the scope of the requester
+   */
+  Policy.any ([
+    Policy.assert ('gatekeeper.request.hasScope', gatekeeper.scope.account.create),
+    Policy.assert ('gatekeeper.isSuperUser')
+  ]),
+
+  /*
+   * Check the password policy. The password policy is optional. If an application
+   * wants to enable password policies, then they just need to overload this policy
+   * in their application.
+   */
+  Policy.assert ('?gatekeeper.account.password')
 ]);
+
+
