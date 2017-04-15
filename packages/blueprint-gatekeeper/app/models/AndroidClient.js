8ce27f84ba7243fd1a3ae537f6643dc9a1c1da8d
James H. Hill <hilljh82@gmail.com>
2017-04-14 23:11:58 -0400

    Fixed Android model definition

--- packages/blueprint-gatekeeper/app/models/AndroidClient.js
+++ packages/blueprint-gatekeeper/app/models/AndroidClient.js
@@ -2,14 +2,27 @@
 
 var uid     = require ('uid-safe')
   , mongodb = require ('@onehilltech/blueprint-mongodb')
-  , Native  = require ('./NativeClient')
+  , Native  = require ('./Client')
   , options = require ('./commonOptions') ()
   ;
 
-options.discriminatorKey = Client.schema.options.discriminatorKey;
+options.discriminatorKey = Native.schema.options.discriminatorKey;
+
+const DEFAULT_SECRET_LENGTH = 128;
 
 var schema = new mongodb.Schema ({
+  /// The client secret, which can be auto-generated.
+  client_secret: {type: String, required: true, validation: {optional: true}},
+
+  /// Android package.
   package: {type: String, required: true}
 }, options);
+
+schema.pre ('validate', function (next) {
+  if (!this.client_secret || this.client_secret === '')
+    this.client_secret = uid.sync (DEFAULT_SECRET_LENGTH);
+
+  return next ();
+});
 
 module.exports = Native.discriminator ('android', schema);
