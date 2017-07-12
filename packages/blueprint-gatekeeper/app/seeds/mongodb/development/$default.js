01ebab552bd09de884004454c0e02143c940d2ca
James H. Hill <hilljh82@gmail.com>
2017-07-12 09:19:59 -0400

    Patched dependencies

--- packages/blueprint-gatekeeper/app/seeds/mongodb/development/$default.js
+++ packages/blueprint-gatekeeper/app/seeds/mongodb/development/$default.js
@@ -16,13 +16,24 @@ module.exports = {
     }
   ],
 
+  android: [
+    {
+      _id: new ObjectId ('593dc15c33812acb3a46ff30'),
+      name: 'gatekeeper-android-demo',
+      client_secret: 'gatekeeper-android-demo',
+      email: 'james@onehilltech.com',
+      package: 'com.onehilltech.gatekeeper.android.examples.standard',
+      scope: [gatekeeper.scope.account.create]
+    }
+  ],
+
   accounts: [
     {
       _id: new ObjectId ('58ed92f64da0861f6aeb98cf'),
       email: 'tester@onehilltech.com',
       username: 'account1',
       password: 'account1',
-      created_by: dab.ref ('native.0')
+      created_by: dab.ref ('android.0')
     }
   ]
 };
