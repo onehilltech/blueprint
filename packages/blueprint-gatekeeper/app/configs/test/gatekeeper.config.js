b5a4fe8587403becdee3668321afa54227719257
James H. Hill <hilljh82@gmail.com>
2017-10-26 02:08:19 -0400

    Fixed test config; regenerated api keys

--- packages/blueprint-gatekeeper/app/configs/test/gatekeeper.config.js
+++ packages/blueprint-gatekeeper/app/configs/test/gatekeeper.config.js
@@ -1,19 +1,9 @@
 'use strict';
 
-const nodemailer = require ('nodemailer')
-  , mailgun      = require ('nodemailer-mailgun-transport')
-  ;
-
-let auth = {
-  auth: {
-    api_key: 'key-af8e0d976bc823c09b77d5f6892ae14b',
-    domain: 'mg.onehilltech.com'
-  }
-};
-
 module.exports = {
   email : {
-    send: true,
-    transport: nodemailer.createTransport (mailgun (auth))
+    transport: {
+      jsonTransport: true
+    }
   }
 };
