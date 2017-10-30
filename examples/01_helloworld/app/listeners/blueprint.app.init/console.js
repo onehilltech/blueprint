a486386bd019246f148cd2cc6d7298bad56d07fe
James H. Hill <hilljh82@gmail.com>
2017-10-30 15:03:42 -0400

    Added listeners to the hello world example

---
+++ examples/01_helloworld/app/listeners/blueprint.app.init/console.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Blueprint = require ('@onehilltech/blueprint')
+  ;
+
+module.exports = Blueprint.Listener.extend ({
+  doEvent () {
+    console.log ('The application is initialized.')
+  }
+});
