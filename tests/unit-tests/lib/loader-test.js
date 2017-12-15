63566f9a45c09421d1f4ecba12f2f3e2e2176b32
James H. Hill <hilljh82@gmail.com>
2017-12-15 16:46:04 -0500

    Switched loader from function to class

--- tests/unit-tests/lib/loader-test.js
+++ tests/unit-tests/lib/loader-test.js
@@ -1,12 +1,13 @@
 const {expect} = require ('chai');
 const path = require ('path');
-const loader = require ('../../../lib/loader');
+const Loader = require ('../../../lib/loader');
 
 describe ('lib | loader', function () {
   it ('should load all the assets', function (done) {
     let dirname = path.resolve ('./tests/fixtures/loader-test');
+    let loader = new Loader ();
 
-    loader ({dirname}).then (assets => {
+    loader.load ({dirname}).then (assets => {
       expect (assets).to.deep.equal ({
         a: { b: {c: 'c-override', d: 'd' } },
         a1: 'a1'
