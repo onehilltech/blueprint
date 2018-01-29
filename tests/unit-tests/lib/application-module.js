327a9972f973c2df039e398b0b1ebbc092ec42fa
James H. Hill <hilljh82@gmail.com>
2018-01-29 01:45:04 -0500

    Application module is able to load listeners

--- tests/unit-tests/lib/application-module.js
+++ tests/unit-tests/lib/application-module.js
@@ -20,9 +20,11 @@ describe ('lib | ApplicationModule', function () {
       appModule.load ().then (result => {
         expect (appModule).to.equal (result);
 
-        expect (result).to.have.nested.property ('_resources.models.person');
-        expect (result).to.have.nested.property ('_resources.models.logger');
-        expect (result).to.not.have.nested.property ('_resources.models.monitor');
+        expect (result._resources).to.have.nested.property ('models.person');
+        expect (result._resources).to.have.nested.property ('models.logger');
+        expect (result._resources).to.not.have.nested.property ('models.monitor');
+
+        expect (result._resources).to.have.property ('listeners').to.have.property ('blueprint.module.init');
 
         done (null);
       }).catch (err => done (err));
