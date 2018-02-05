98c93761fab223a2550144e2a15ac4b62bd817af
James H. Hill <hilljh82@gmail.com>
2018-02-05 17:00:29 -0500

    Export the token generator

--- packages/blueprint-gatekeeper/lib/index.js
+++ packages/blueprint-gatekeeper/lib/index.js
@@ -2,3 +2,4 @@ exports.scope = require ('./scope');
 exports.tokens = require ('./tokens');
 exports.middleware = require ('../app/middleware');
 exports.testing = require ('./testing');
+exports.TokenGenerator = require ('../app/utils/token-generator');
