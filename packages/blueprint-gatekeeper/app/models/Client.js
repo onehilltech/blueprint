60d98a335dbe1371f10b141c91c1ad97e0040e06
James H. Hill <hilljh82@gmail.com>
2018-02-12 12:39:09 -0500

    Refactored logic for claiming a device

--- packages/blueprint-gatekeeper/app/models/Client.js
+++ packages/blueprint-gatekeeper/app/models/Client.js
@@ -1,13 +1,16 @@
-'use strict';
+const mongodb = require ('@onehilltech/blueprint-mongodb');
+const options = require ('./commonOptions') ();
+const Account = require ('./Account');
 
-var uid     = require ('uid-safe')
-  , mongodb = require ('@onehilltech/blueprint-mongodb')
-  , options = require ('./commonOptions') ()
-  ;
+const {
+  Schema: {
+    Types: {ObjectId}
+  }
+} = mongodb;
 
 options.discriminatorKey = 'type';
 
-var schema = new mongodb.Schema ({
+let schema = new Schema ({
   /// The type of client.
   type: {type: String, required: true},
 
@@ -22,7 +25,19 @@ var schema = new mongodb.Schema ({
 
   /// The default scope for the client. The scope is applied to the access
   /// token for the client.
-  scope: {type: [String], default: []}
+  scope: {type: [String], default: []},
+
+  /// The client is private. A private client can only be accessed by the user
+  /// associated with the email for this client, and those that appear in the
+  /// whitelist.
+  private: {type: Boolean, default: false},
+
+  /// Accounts allowed to use the client. This list is used when the client
+  /// is marked private.
+  allow: {type: [ObjectId], ref: Account.modelName},
+
+  /// Accounts not allowed to use the client. This list is always used.
+  deny: {type: [ObjectId], ref: Account.modelName}
 }, options);
 
 const MODEL_NAME = 'client';
