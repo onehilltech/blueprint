3ad35c5192bc272d7fc83923218bac010c1ed7a2
James H. Hill <hilljh82@gmail.com>
2018-03-07 00:06:00 -0500

    Fixed errors in test cases

--- tests/unit-tests/lib/environment-test.js
+++ tests/unit-tests/lib/environment-test.js
@@ -1,8 +1,8 @@
-const Env = require ('../../../lib/environment');
+const {env} = require ('../../../lib/environment');
 const expect = require ('chai').expect;
 
 describe ('lib | Environment', function () {
   it ('should create an environment object', function () {
-    expect (Env.name).to.equal ('test');
+    expect (env).to.equal ('test');
   });
 });
