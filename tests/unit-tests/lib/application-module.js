3aae14c1b7e26e6ce0d0d378c4d5e6c10025e7be
James H. Hill <hilljh82@gmail.com>
2018-03-07 01:41:42 -0500

    Changed load() => configure()

--- tests/unit-tests/lib/application-module.js
+++ tests/unit-tests/lib/application-module.js
@@ -3,7 +3,7 @@ const path = require ('path');
 const {expect} = require ('chai');
 
 describe ('lib | ApplicationModule', function () {
-  describe ('constructor()', function () {
+  describe ('constructor', function () {
     it ('should create a new ApplicationModule', function () {
       let modulePath = path.resolve (__dirname, '../../fixtures/app-module');
       let appModule = new ApplicationModule ({modulePath});
@@ -12,12 +12,12 @@ describe ('lib | ApplicationModule', function () {
     });
   });
 
-  describe ('load()', function () {
+  describe ('configure', function () {
     it ('should load an application module into memory', function (done) {
       let modulePath = path.resolve (__dirname, '../../fixtures/app-module');
       let appModule = new ApplicationModule ({modulePath});
 
-      appModule.load ().then (result => {
+      appModule.configure ().then (result => {
         expect (appModule).to.equal (result);
 
         expect (result._resources).to.have.nested.property ('models.person');
