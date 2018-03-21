6ed735296f2de478a9a49bea25bd0fcf71a0d8da
James H. Hill <hilljh82@gmail.com>
2018-03-21 17:47:18 -0400

    First pass at updating cluster design

---
+++ examples/05_cluster/app/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const blueprint = require ('@onehilltech/blueprint');
+
+blueprint.createApplicationAndStart (__dirname).then (() => {
+  console.log ('The application is started.');
+}).catch (err => {
+  console.error (err);
+});
