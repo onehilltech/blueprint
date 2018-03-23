49e52eafd89b726dc83d956812b5b02cbd35a44e
James H. Hill <hilljh82@gmail.com>
2018-03-22 21:34:12 -0400

    Added support for all() policy checks

---
+++ tests/unit-tests/lib/policies/all-test.js
@@ -0,0 +1,69 @@
+const {
+  expect
+} = require ('chai');
+
+const all = require ('../../../../lib/policies/all');
+const check = require ('../../../../lib/policies/check');
+const Policy = require ('../../../../lib/policy');
+
+describe ('lib | policies | all', function () {
+  let policies = {
+    identity (value) {
+      return new Policy ({
+        runCheck () { return value; }
+      });
+    }
+  };
+
+  it ('should pass all policies', function (done) {
+    let a = all ([
+      check ('identity', true),
+      check ('identity', true)
+    ]);
+
+    let policy = a.resolvePolicyFrom (policies);
+
+    policy.runCheck ().then (result => {
+      expect (result).to.be.true;
+      return done (null);
+    }).catch (done);
+  });
+
+  it ('should fail since one policy fails', function (done) {
+    let a = all ([
+      check ('identity', true),
+      check ('identity', false)
+    ]);
+
+    let policy = a.resolvePolicyFrom (policies);
+
+    policy.runCheck ()
+      .then (result => {
+        expect (result).to.deep.equal ({
+          failureCode: "policy_failed",
+          failureMessage: "The request did not satisfy a required policy."
+        });
+
+        done (null);
+      }).catch (done);
+  });
+
+  it ('should support nested policies', function (done) {
+    let a = all ([
+      check ('identity', true),
+      check ('identity', true),
+
+      all ([
+        check ('identity', true),
+        check ('identity', true),
+      ])
+    ]);
+
+    let policy = a.resolvePolicyFrom (policies);
+
+    policy.runCheck ().then (result => {
+      expect (result).to.be.true;
+      return done (null);
+    }).catch (done);
+  });
+});
