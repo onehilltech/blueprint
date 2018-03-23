a48c4d2b433f8d1c322bff2ae7d3b24549f08c5a
James H. Hill <hilljh82@gmail.com>
2018-03-22 22:37:05 -0400

    Added support for negating a policy

---
+++ tests/unit-tests/lib/policies/-negate-test.js
@@ -0,0 +1,47 @@
+const {
+  expect
+} = require ('chai');
+
+const all = require ('../../../../lib/policies/-negate');
+const check = require ('../../../../lib/policies/check');
+const Policy = require ('../../../../lib/policy');
+
+describe ('lib | policies | -negate', function () {
+  let policies = {
+    identity (value) {
+      return new Policy ({
+        runCheck () { return value; }
+      });
+    }
+  };
+
+  it ('should negate a true to false', function (done) {
+    let c = check ('!identity', true);
+    let policy = c.resolvePolicyFrom (policies);
+
+    policy.runCheck ().then (result => {
+      expect (result).to.be.false;
+      return done (null);
+    }).catch (done);
+  });
+
+  it ('should negate a false to true', function (done) {
+    let c = check ('!identity', false);
+    let policy = c.resolvePolicyFrom (policies);
+
+    policy.runCheck ().then (result => {
+      expect (result).to.be.true;
+      return done (null);
+    }).catch (done);
+  });
+
+  it ('should negate a failure object to true', function (done) {
+    let c = check ('!identity', {failureCode: 'failed', failureMessage: 'The message'});
+    let policy = c.resolvePolicyFrom (policies);
+
+    policy.runCheck ().then (result => {
+      expect (result).to.be.true;
+      return done (null);
+    }).catch (done);
+  });
+});
