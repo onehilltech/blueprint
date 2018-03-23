202e2c593eb5bac3859722bfd7ea56210095615d
James H. Hill <hilljh82@gmail.com>
2018-03-22 22:44:39 -0400

    Update policies example

--- examples/02_policies/app/routers/main.js
+++ examples/02_policies/app/routers/main.js
@@ -1,7 +1,5 @@
 const {
-  policies: {
-    check
-  }
+  policies: { check, all, any }
 } = require ('@onehilltech/blueprint');
 
 module.exports = {
@@ -11,8 +9,37 @@ module.exports = {
     get  : { view : 'helloworld.pug'},
 
     post : {
-      policy: check ('identity', false),
+      policy: all ([
+        check ('identity', true),
+        check ('identity', false)
+      ]),
+
       action : 'helloworld@echoName'
     },
+
+    '/negate': {
+      get: {
+        policy: check ('!identity', false),
+        view: 'helloworld.pug'
+      }
+    },
+
+    '/optional': {
+      get: {
+        policy: check ('?missing', false),
+        view: 'helloworld.pug'
+      }
+    },
+
+    '/any': {
+      get: {
+        policy: any ([
+          check ('identity', false),
+          check ('identity', false)
+        ]),
+        view: 'helloworld.pug'
+      }
+    }
+
   }
 };
