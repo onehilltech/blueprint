81e7ded8dc7c0f719910f3175594dfcd7c0a089a
James H. Hill <hilljh82@gmail.com>
2018-04-03 19:56:23 -0400

    Refactored exports

--- packages/blueprint-testing/lib/test.js
+++ packages/blueprint-testing/lib/test.js
@@ -1,7 +1,7 @@
 const path = require ('path');
 const blueprint = require ('@onehilltech/blueprint');
 
-function blueprintTest (description, test) {
+function test (description, test) {
   // As part of the testing framework, we need to make sure we create/start
   // and destroy the application before and after each test, respectively.
   // We are assuming the dummy application is under ./tests/dummy/app.
@@ -19,4 +19,4 @@ function blueprintTest (description, test) {
   describe (description, test);
 }
 
-module.exports = blueprintTest;
+module.exports = test;
