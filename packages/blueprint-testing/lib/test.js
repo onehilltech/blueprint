32d2e30ee7c125ac79d7c27d6d28fc20384e32ba
James H. Hill <hilljh82@gmail.com>
2018-04-04 00:13:24 -0400

    Fixed errors in test module

--- packages/blueprint-testing/lib/test.js
+++ packages/blueprint-testing/lib/test.js
@@ -1,22 +1,13 @@
 const path = require ('path');
 const blueprint = require ('@onehilltech/blueprint');
 
-function test (description, test) {
-  // As part of the testing framework, we need to make sure we create/start
-  // and destroy the application before and after each test, respectively.
-  // We are assuming the dummy application is under ./tests/dummy/app.
+// The dummy application has a well-defined location.
+const appPath = path.resolve ('./tests/dummy/app');
 
-  const appPath = path.resolve ('./tests/dummy/app');
+beforeEach (function () {
+  return blueprint.createApplicationAndStart (appPath);
+});
 
-  beforeEach (function () {
-    return blueprint.createApplicationAndStart (appPath);
-  });
-
-  afterEach (function () {
-    return blueprint.destroyApplication ();
-  });
-
-  describe (description, test);
-}
-
-module.exports = test;
+afterEach (function () {
+  return blueprint.destroyApplication ();
+});
