ba2e70ecaa8642e0d104d4075467abc8272b8ece
James H. Hill <hilljh82@gmail.com>
2018-04-12 01:30:19 -0400

    Added mixin support to objects

--- tests/unit-tests/lib/object-test.js
+++ tests/unit-tests/lib/object-test.js
@@ -1,4 +1,6 @@
 const BlueprintObject = require ('../../../lib/object');
+const Mixin = require ('../../../lib/mixin');
+
 const { expect } = require ('chai');
 
 describe ('lib | BlueprintObject', function () {
@@ -7,6 +9,36 @@ describe ('lib | BlueprintObject', function () {
   context ('class', function () {
     it ('should not have functions from objectPath', function () {
       expect (BlueprintObject).to.not.include.keys (keys);
+    });
+  });
+
+  context ('extend', function () {
+    it ('should extend base object with no additional properties', function () {
+      const A = BlueprintObject.extend ();
+      expect (A).to.be.a ('function');
+    });
+
+    it ('should extend base object with properties', function () {
+      const A = BlueprintObject.extend ({
+        a () { },
+        b () { }
+      });
+
+      expect (A.prototype).to.have.keys (['a','b']);
+    });
+
+    it ('should extend base object with mixins and properties', function () {
+      const M = Mixin.create ({
+        z () {}
+      });
+
+      const A = BlueprintObject.extend (M, {
+        a () { },
+        b () { }
+      });
+
+      expect (A.prototype).to.have.keys (['a','b','z']);
+
     });
   });
 
