79c6b8d7cca9d2fb60958f81889f3761eb50e99c
James H. Hill <hilljh82@gmail.com>
2018-04-14 01:05:33 -0400

    Fixed all test errors related to removal of core-object library

--- tests/unit-tests/lib/array-upload-action-test.js
+++ tests/unit-tests/lib/array-upload-action-test.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const ArrayUploadAction = require ('../../../lib/array-upload-action');
 const executeAction = require ('../../../lib/middleware/execute-action');
 
@@ -20,7 +36,7 @@ describe ('lib | ArrayUploadAction', function () {
 
   describe ('execute', function () {
     it ('should upload an array of files', function () {
-      let action = new ArrayUploadAction ({
+      let action = ArrayUploadAction.create ({
         uploadPath: './temp',
         name: 'avatar',
         uploadCompleteCalled: false,
@@ -44,7 +60,7 @@ describe ('lib | ArrayUploadAction', function () {
       let app = express ();
       app.post ('/profile', executeAction (action));
 
-      const avatarPng = path.resolve (__dirname, '../../files/avatar.png');
+      const avatarPng = path.resolve ('./tests/files/avatar.png');
 
       return request (app)
         .post ('/profile')
