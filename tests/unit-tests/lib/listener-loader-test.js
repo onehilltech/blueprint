a25b654f7a5757e623032cdc76c498b8b1bb775a
James H. Hill <hilljh82@gmail.com>
2018-04-14 09:49:20 -0400

    The ListenerLoader needs a target messenger, not an application

--- tests/unit-tests/lib/listener-loader-test.js
+++ tests/unit-tests/lib/listener-loader-test.js
@@ -16,27 +16,23 @@
 
 const {expect} = require ('chai');
 const ListenerLoader = require ('../../../lib/listener-loader');
-const messaging = require ('../../../lib/messaging');
+const BlueprintObject = require ('../../../lib/object');
+
+const {Events} = require ('../../../lib/messaging');
 const path = require ('path');
 
 describe ('lib | ListenerLoader', function () {
   describe ('load', function () {
     it ('should load listeners', function () {
       const dirname = path.resolve (__dirname, '../../dummy/app/listeners');
-
-      let app = { messaging: messaging () };
-      let loader = new ListenerLoader ({app});
+      let messenger = BlueprintObject.create (Events);
+      let loader = new ListenerLoader ({messenger});
 
       return loader.load ({dirname}).then (results => {
         expect (results).to.have.property ('blueprint.app.init').that.has.keys (['echo','legacy']);
 
-        expect (app.messaging)
-          .to.have.property ('messengers')
-          .to.have.property ('_')
-          .to.have.property ('_eventListeners')
-          .to.have.property ('blueprint.app.init')
-          .to.have.property ('_on')
-          .to.have.length (2);
+        expect (messenger.hasListeners ('blueprint.app.init')).to.be.true;
+        expect (messenger.getListeners ('blueprint.app.init')).to.have.length (2);
       });
     });
   })
