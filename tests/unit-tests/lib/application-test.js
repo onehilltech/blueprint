48d9cc0ff7637e9f9d10617b11fc76805afdcd71
James H. Hill <hilljh82@gmail.com>
2018-04-14 13:31:50 -0400

    Remove the messaging framework class

--- tests/unit-tests/lib/application-test.js
+++ tests/unit-tests/lib/application-test.js
@@ -1,16 +1,30 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const { expect } = require ('chai');
 const path = require ('path')
 const Application = require ('../../../lib/application');
-const messaging   = require ('../../../lib/messaging');
 
 const express = require ('express');
 const request = require ('supertest');
 
 describe ('lib | Application', function () {
-
   function makeApplication () {
     const appPath = path.resolve ('./tests/dummy/app');
-    return new Application ({appPath, messaging: messaging ()});
+    return new Application ({appPath});
   }
 
   describe ('configure', function () {
--- tests/unit-tests/lib/module-loader-test.js
+++ tests/unit-tests/lib/module-loader-test.js
@@ -1,19 +1,32 @@
-const {expect} = require ('chai');
-const path     = require ('path');
-const ModuleLoader = require ('../../../lib/module-loader');
-const messaging = require ('../../../lib/messaging');
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-const appPath = path.resolve (__dirname, '../../dummy/app');
+const {expect} = require ('chai');
+const path = require ('path');
+const ModuleLoader = require ('../../../lib/module-loader');
+const Application = require ('../../../lib/application');
+const appPath = path.resolve ('./tests/dummy/app');
+const BlueprintObject = require ('../../../lib/object');
+const Events = require ('../../../lib/messaging/events');
 
 describe ('lib | ModuleLoader', function () {
   describe ('constructor', function () {
     it ('should create a ModuleLoader', function () {
-      let m = new ModuleLoader ({
-        app: {
-          appPath,
-          messaging: messaging ()
-        }
-      });
+      let app = new Application ({appPath});
+      let m = new ModuleLoader ({app})
 
       expect (m).to.not.be.null;
     });
@@ -21,11 +34,10 @@ describe ('lib | ModuleLoader', function () {
 
   describe ('load', function () {
     it ('should the application modules', function () {
-      let app = new Object ({
+      // Make a mock application for the loader.
+      let app = BlueprintObject.create (Events, {
         appPath,
-        messaging: messaging (),
         modules: {},
-
         addModule (name, module) {
           this.modules[name] = module;
         }
