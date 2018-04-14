74bf3e286a446965ea5701a9939b1da0216a3ac7
James H. Hill <hilljh82@gmail.com>
2018-04-14 00:46:36 -0400

    Fixed test cases

--- tests/unit-tests/lib/listener-loader-test.js
+++ tests/unit-tests/lib/listener-loader-test.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const {expect} = require ('chai');
 const ListenerLoader = require ('../../../lib/listener-loader');
 const messaging = require ('../../../lib/messaging');
@@ -17,7 +33,7 @@ describe ('lib | ListenerLoader', function () {
         expect (app.messaging)
           .to.have.property ('messengers')
           .to.have.property ('_')
-          .to.have.property ('_listeners')
+          .to.have.property ('_eventListeners')
           .to.have.property ('blueprint.app.init')
           .to.have.property ('_on')
           .to.have.length (2);
