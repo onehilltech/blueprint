473c5b1e80baa3fd42ec4988f5afd7a1f7804874
James H. Hill <hilljh82@gmail.com>
2018-04-13 23:43:51 -0400

    Simplified the implementation

--- packages/blueprint/lib/controller.js
+++ packages/blueprint/lib/controller.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const BlueprintObject = require ('./object');
 const ApplicationMessaging = require ('./application-messaging');
 
@@ -6,10 +22,4 @@ const ApplicationMessaging = require ('./application-messaging');
  *
  * Base class for all controllers.
  */
-module.exports = BlueprintObject.extend ({
-  init () {
-    this._super.init.apply (this, arguments);
-
-    BlueprintObject.mixin (this, ApplicationMessaging);
-  }
-});
+module.exports = BlueprintObject.extend (ApplicationMessaging);
