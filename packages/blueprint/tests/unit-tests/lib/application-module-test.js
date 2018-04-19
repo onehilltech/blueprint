f930250488feaf2434f64785e7a2fc36c15bb6e4
James H. Hill <hilljh82@gmail.com>
2018-04-19 18:29:53 -0400

    Enabled tests for ApplicationModule

--- packages/blueprint/tests/unit-tests/lib/application-module-test.js
+++ packages/blueprint/tests/unit-tests/lib/application-module-test.js
@@ -14,65 +14,66 @@
  * limitations under the License.
  */
 
-const path     = require ('path');
-const {expect} = require ('chai');
-const ApplicationModule = require ('../../../lib/application-module');
+const path = require ('path');
 
-describe.skip ('lib | ApplicationModule', function () {
+const {
+  expect
+} = require ('chai');
+
+const ApplicationModule = require ('../../../lib/application-module');
+const blueprint = require ('../../../lib');
+
+describe ('lib | ApplicationModule', function () {
   describe ('constructor', function () {
     it ('should create a new ApplicationModule', function () {
-      let appPath = path.resolve (__dirname, '../../fixtures/app-module');
+      let app = blueprint.app;
+      let appModule = new ApplicationModule ({modulePath: app.appPath, app});
 
-      let appModule = new ApplicationModule ({
-        appPath,
-        messaging: new MessagingFramework ()
-      });
-
-      expect (appModule).to.deep.include ({appPath, _resources: {}});
+      expect (appModule).to.deep.include ({modulePath: app.appPath});
     });
   });
 
   describe ('configure', function () {
     it ('should load an application module into memory', function () {
-      let appPath = path.resolve (__dirname, '../../fixtures/app-module');
-      let appModule = new ApplicationModule ({
-        appPath,
-        messaging: new MessagingFramework ()
-      });
+      let app = blueprint.app;
+      let appModule = new ApplicationModule ({app, modulePath: app.appPath});
 
       return appModule.configure ().then (result => {
         expect (appModule).to.equal (result);
 
-        expect (result._resources).to.have.nested.property ('models.person');
-        expect (result._resources).to.have.nested.property ('models.logger');
-        expect (result._resources).to.not.have.nested.property ('models.monitor');
+        expect (result.resources).to.have.nested.property ('controllers.main');
+        expect (result.resources).to.have.nested.property ('controllers.namespace-user');
+        expect (result.resources).to.have.nested.property ('controllers.user');
 
-        expect (result._resources).to.have.nested.property ('controllers.module-test');
+        expect (result.resources).to.have.nested.property ('listeners.blueprint\\.app\\.init');
 
-        expect (result._resources).to.have.property ('listeners').to.have.property ('blueprint.module.init');
+        expect (result.resources).to.have.nested.property ('models.person');
+
+        expect (result.resources).to.have.nested.property ('policies.identity');
+
+        expect (result.resources).to.have.nested.property ('services.cart');
+        expect (result.resources).to.have.nested.property ('services.shopping-cart');
+
+        expect (result.resources).to.have.nested.property ('routers.main');
+        expect (result.resources).to.have.nested.property ('routers.users');
+        expect (result.resources).to.have.nested.property ('routers.inner.main');
       });
     });
   });
 
   describe ('viewsPath', function () {
     it ('should get the viewPaths property', function () {
-      let appPath = path.resolve (__dirname, '../../dummy/app');
-      let appModule = new ApplicationModule ({
-        appPath,
-        messaging: new MessagingFramework ()
-      });
+      let app = blueprint.app;
+      let appModule = new ApplicationModule ({app, modulePath: app.appPath});
 
-      expect (appModule.viewsPath).to.equal (path.join (appPath, 'views'));
+      expect (appModule.viewsPath).to.equal (path.join (app.appPath, 'views'));
     });
   });
 
   describe ('hasViews', function () {
     it ('should test if the module has views', function () {
-      let appPath = path.resolve (__dirname, '../../dummy/app');
-      let appModule = new ApplicationModule ({
-        appPath,
-        messaging: new MessagingFramework ()
-      });
+      let app = blueprint.app;
+      let appModule = new ApplicationModule ({app, modulePath: app.appPath});
 
       expect (appModule.hasViews).to.be.true;
     });
@@ -80,16 +81,13 @@ describe.skip ('lib | ApplicationModule', function () {
 
   describe ('lookup', function () {
     it ('should lookup an entity', function () {
-      let appPath = path.resolve (__dirname, '../../dummy/app');
-      let appModule = new ApplicationModule ({
-        appPath,
-        messaging: new MessagingFramework ()
-      });
+      let app = blueprint.app;
+      let appModule = new ApplicationModule ({app, modulePath: app.appPath});
 
       return appModule.configure ()
         .then (() => {
-          let controller = appModule.lookup ('controller:MainController');
-          expect (controller).to.not.be.undefined;
+          let controller = appModule.lookup ('controller:main');
+          expect (controller).to.equal (appModule.resources.controllers.main);
         });
     });
   });
