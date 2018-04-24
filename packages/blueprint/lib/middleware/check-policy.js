fa9ce986c6dfe5b5372f4a6311ab2b6d5b075085
James H. Hill <hilljh82@gmail.com>
2018-04-24 00:41:48 -0400

    Re-implemented the check policy to resolve result from runCheck()

--- packages/blueprint/lib/middleware/check-policy.js
+++ packages/blueprint/lib/middleware/check-policy.js
@@ -1,4 +1,21 @@
-const HttpError = require ('../http-error');
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const InternalServerError = require ('../internal-server-error');
+const ForbiddenError = require ('../forbidden-error');
 
 const {
   isObjectLike,
@@ -8,38 +25,32 @@ const {
  * Factory method for creating a policy middleware for a request.
  *
  * @param policy          A Policy object
- * @param params          Optional parameters for the policy
  * @returns {Function}
  */
 module.exports = function (policy) {
   return function __blueprint_check_policy (req, res, next) {
     let result = policy.runCheck (req);
 
-    if ((result instanceof Promise))
-      result.then (result => checked (result)).catch (next);
-    else
-      checked (result);
-
-    function checked (result) {
+    return Promise.resolve (result).then (result => {
       if (result === true)
         return next ();
 
       if (result === false) {
         // The policy result was a boolean value. This means that we are to use
         // the default failure code and message as the policy error.
-        const {failureCode,failureMessage} = policy;
-        return next (new HttpError (403, failureCode, failureMessage));
+        const {failureCode, failureMessage} = policy;
+        return next (new ForbiddenError (failureCode, failureMessage));
       }
       else if (isObjectLike (result)) {
         // The result is an object. This means we are to use the result
         // as-is for the policy error.
         const {failureCode, failureMessage} = result;
-        return next (new HttpError (403, failureCode, failureMessage));
+        return next (new ForbiddenError (failureCode, failureMessage));
       }
       else {
         console.error (`Policy ${policy.name} returned a bad result.`);
-        return next (new HttpError (500, 'bad_result', 'The policy returned a bad result.'));
+        return next (new InternalServerError ('bad_result', 'The policy returned a bad result.'));
       }
-    }
+    });
   };
 };
