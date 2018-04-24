049e8c743c9a4af9fe3d72616420d67caf03949f
James H. Hill <hilljh82@gmail.com>
2018-04-24 00:36:43 -0400

    Added concrete implementations of http errors

---
+++ packages/blueprint/lib/forbidden-error.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const HttpError = require ('./http-error');
+
+/**
+ * @class HttpError
+ *
+ * Base class for all http errors.
+ *
+ * @param statusCode      Http status code
+ * @param code            Error code
+ * @param message         Error message
+ * @param details         Optional details about the error
+ * @constructor
+ */
+class ForbiddenError extends HttpError {
+  constructor (code, message, details) {
+    super (403, code, message, details);
+  }
+}
+
+module.exports = ForbiddenError;
