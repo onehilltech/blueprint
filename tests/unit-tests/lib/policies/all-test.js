80dd667fbe8d1eb830ec3222486ee3bf86d740a8
James H. Hill <hilljh82@gmail.com>
2018-05-04 16:09:32 -0400

    allSeries => all.ordered

--- tests/unit-tests/lib/policies/all-test.js
+++ tests/unit-tests/lib/policies/all-test.js
@@ -1,10 +1,26 @@
-const {
-  expect
-} = require ('chai');
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-const all = require ('../../../../lib/policies/all');
-const check = require ('../../../../lib/policies/check');
+const { expect } = require ('chai');
 const blueprint = require ('../../../../lib');
+
+const {
+  Policy,
+  policies: {all, check}
+} = blueprint;
 
 describe ('lib | policies | all', function () {
   it ('should pass all policies', function () {
@@ -51,6 +67,38 @@ describe ('lib | policies | all', function () {
 
     return policy.runCheck ().then (result => {
       expect (result).to.be.true;
+    });
+  });
+
+  context ('ordered', function () {
+    const SimplePolicy = Policy.extend ({
+      value: null,
+
+      runCheck (req) {
+        req.values.push (this.value);
+        return true;
+      }
+    });
+
+    it ('should execute the policies in order', function () {
+      let policies = [
+        new SimplePolicy ({value: 1}),
+        new SimplePolicy ({value: 2}),
+        new SimplePolicy ({value: 3}),
+        new SimplePolicy ({value: 4})
+      ];
+
+      let req = {
+        values: []
+      };
+
+      const Policy = all.ordered (policies);
+      const policy = new Policy ();
+
+      return policy.runCheck (req).then (result => {
+        expect (result).to.be.true;
+        expect (req.values).to.eql ([1, 2, 3, 4]);
+      })
     });
   });
 });
