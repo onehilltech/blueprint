be2444604d5020e92ce2deb471eb006ea590610d
James H. Hill <hilljh82@gmail.com>
2018-05-08 15:15:26 -0400

    Removed debug

--- packages/blueprint-cli/templates/blueprint-new/tests/unit-tests/bootstrap.js
+++ packages/blueprint-cli/templates/blueprint-new/tests/unit-tests/bootstrap.js
@@ -1,6 +1,5 @@
 const path  = require ('path');
 const blueprint = require ('@onehilltech/blueprint');
-const debug = require ('debug')('bootstrap');
 
 before (function () {
   const appPath = path.resolve (__dirname, '../dummy/app');
@@ -8,12 +7,10 @@ before (function () {
 });
 
 beforeEach (function () {
-  debug ('starting a new test');
   return blueprint.emit ('blueprint.test.start')
 });
 
 afterEach (function () {
-  debug ('ending the test');
   return blueprint.emit ('blueprint.test.complete');
 });
 
