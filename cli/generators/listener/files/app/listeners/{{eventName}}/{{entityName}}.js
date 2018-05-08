d03e1bc82ff869d79283cfc84e31b4fa8c73cef4
James H. Hill <hilljh82@gmail.com>
2018-05-07 20:11:26 -0400

    Added listener generator

---
+++ cli/generators/listener/files/app/listeners/{{eventName}}/{{entityName}}.js
@@ -0,0 +1,7 @@
+const { Listener } = require ('@onehilltech/blueprint');
+
+module.exports = Listener.extend ({
+  handleEvent (/* arg0, arg1, arg2*/) {
+    // TODO Add implementation here
+  }
+});
