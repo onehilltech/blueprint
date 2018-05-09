5bbaa5fca967e731f11a4b391fb8ba2324e22b03
James H. Hill <hilljh82@gmail.com>
2018-05-09 10:00:28 -0400

    toDate failed if number was a float

--- packages/blueprint-mongodb/app/sanitizers/toDate.js
+++ packages/blueprint-mongodb/app/sanitizers/toDate.js
@@ -15,15 +15,9 @@
  */
 
 const moment = require ('moment');
-
-const {
-  isNumeric,
-  isJSON
-} = require ('validator');
-
-const {
-  parse
-} = JSON;
+const { isNumeric, isJSON } = require ('validator');
+const { parse } = JSON;
+const { toNumber } = require ('lodash');
 
 module.exports = function (value, opts = {}) {
   if (value === undefined || value === null)
@@ -31,31 +25,33 @@ module.exports = function (value, opts = {}) {
 
   let m = null;
 
-  if (isNumeric (value)) {
-    let n = parseInt (value);
-    m = moment (n);
-  }
-  else if (isJSON (value)) {
-    let obj = parse (value);
-    m = moment (obj);
-  }
-  else {
-    if (opts.format) {
-      switch (opts.format) {
-        case 'utc':
-          m = moment.utc (value);
-          break;
+  const hasFormat = !!opts.format;
 
-        case 'seconds':
-          m = moment.unix (value);
-          break;
-
-        default:
-          throw new Error (`Unknown format: ${opts.format}`);
-      }
+  if (!hasFormat) {
+    if (isNumeric (value)) {
+      let n = toNumber (value);
+      m = moment (n);
+    }
+    else if (isJSON (value)) {
+      let obj = parse (value);
+      m = moment (obj);
     }
     else {
       m = moment (value);
+    }
+  }
+  else {
+    switch (opts.format) {
+      case 'utc':
+        m = moment.utc (value);
+        break;
+
+      case 'seconds':
+        m = moment.unix (value);
+        break;
+
+      default:
+        throw new Error (`Unknown format: ${opts.format}`);
     }
   }
 
