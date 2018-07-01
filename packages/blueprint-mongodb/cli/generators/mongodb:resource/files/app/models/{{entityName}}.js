7d97766214f4d69629475abb5fdc2bb04115aa57
James H. Hill <hilljh82@gmail.com>
2018-07-01 10:59:55 -0400

    Added support for soft delete

--- packages/blueprint-mongodb/cli/generators/mongodb:resource/files/app/models/{{entityName}}.js
+++ packages/blueprint-mongodb/cli/generators/mongodb:resource/files/app/models/{{entityName}}.js
@@ -1,26 +1,22 @@
-/*
- * Copyright (c) 2018 One Hill Technologies, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 const mongodb = require ('@onehilltech/blueprint-mongodb');
 const { Schema } = mongodb;
 
 // use mongodb.Types to access mongoose.Types
 
-const schema = Schema ({
-  // add your schema definition here
-});
+const options = {
+{{#if collectionName}}
+  // Name of the model collection.
+  collection: '{{collectionName}}',
+{{/if}}
 
-module.exports = mongodb.resource ('{{entityBaseName}}', schema{{#if collectionName}}, '{{collectionName}}'{{/if}});
+{{#if softDelete}}
+  // Support soft delete of the resource.
+  softDelete: true,
+{{/if}}
+};
+
+const schema = new Schema ({
+  // add your schema definition here
+}, options);
+
+module.exports = mongodb.resource ('{{entityBaseName}}', schema);
