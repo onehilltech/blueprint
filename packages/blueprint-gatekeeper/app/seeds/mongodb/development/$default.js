c796e5e39c3a47cf3648c044129676046ad3d95c
James H. Hill <hilljh82@gmail.com>
2018-07-03 00:52:33 -0400

    Updated seed definitions

--- packages/blueprint-gatekeeper/app/seeds/mongodb/development/$default.js
+++ packages/blueprint-gatekeeper/app/seeds/mongodb/development/$default.js
@@ -1,44 +1,68 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const { Seed } = require ('@onehilltech/blueprint-mongodb');
 const dab = require ('@onehilltech/dab');
 
-module.exports = {
-  native: [
-    {
-      _id: dab.id ('58ed90e1105aee00001e429f'),
-      name: 'gatekeeper-android',
-      client_secret: 'gatekeeper-android',
-      email: 'james@onehilltech.com',
-      scope: ['gatekeeper.account.create']
-    }
-  ],
+/**
+ * @class
+ */
+module.exports = Seed.extend ({
+  model () {
+    return {
+      native: [
+        {
+          _id: dab.id ('58ed90e1105aee00001e429f'),
+          name: 'gatekeeper-android',
+          client_secret: 'gatekeeper-android',
+          email: 'james@onehilltech.com',
+          scope: ['gatekeeper.account.create']
+        }
+      ],
 
-  android: [
-    {
-      _id: dab.id ('593dc15c33812acb3a46ff30'),
-      name: 'gatekeeper-android-demo',
-      client_secret: 'gatekeeper-android-demo',
-      email: 'james@onehilltech.com',
-      package: 'com.onehilltech.gatekeeper.android.examples.standard',
-      scope: ['gatekeeper.account.create']
-    }
-  ],
+      android: [
+        {
+          _id: dab.id ('593dc15c33812acb3a46ff30'),
+          name: 'gatekeeper-android-demo',
+          client_secret: 'gatekeeper-android-demo',
+          email: 'james@onehilltech.com',
+          package: 'com.onehilltech.gatekeeper.android.examples.standard',
+          scope: ['gatekeeper.account.create']
+        }
+      ],
 
-  recaptcha: [
-    {
-      _id: dab.id ('5a206991201dc8357e45d174'),
-      name: 'gatekeeper-recaptcha',
-      recaptcha_secret: '6LdcLDcUAAAAAL8U9Im2z-kebfr9M1oqL1lLS0C7',
-      email: 'james@onehilltech.com',
-      scope: ['gatekeeper.account.create']
-    }
-  ],
+      recaptcha: [
+        {
+          _id: dab.id ('5a206991201dc8357e45d174'),
+          name: 'gatekeeper-recaptcha',
+          recaptcha_secret: '6LdcLDcUAAAAAL8U9Im2z-kebfr9M1oqL1lLS0C7',
+          email: 'james@onehilltech.com',
+          scope: ['gatekeeper.account.create']
+        }
+      ],
 
-  accounts: [
-    {
-      _id: dab.id ('58ed92f64da0861f6aeb98cf'),
-      email: 'tester@onehilltech.com',
-      username: 'account1',
-      password: 'account1',
-      created_by: dab.ref ('android.0')
-    }
-  ]
-};
+      accounts: [
+        {
+          _id: dab.id ('58ed92f64da0861f6aeb98cf'),
+          email: 'tester@onehilltech.com',
+          username: 'account1',
+          password: 'account1',
+          created_by: dab.ref ('android.0')
+        }
+      ]
+    };
+  }
+});
--- packages/blueprint-gatekeeper/tests/dummy/app/seeds/mongodb/$default.js
+++ packages/blueprint-gatekeeper/tests/dummy/app/seeds/mongodb/$default.js
@@ -14,109 +14,117 @@
  * limitations under the License.
  */
 
+const { Seed } = require ('@onehilltech/blueprint-mongodb');
 const dab = require ('@onehilltech/dab');
 
-module.exports = {
-  native: [
-    {
-      name: 'client1',
-      email: 'client1@gatekeeper.com',
-      client_secret: 'client1'
-    },
-    {
-      name: 'client2',
-      email: 'client2@gatekeeper.com',
-      client_secret: 'client2'
-    },
-    {
-      name: 'client3',
-      email: 'client3@gatekeeper.com',
-      client_secret: 'client3',
-      enabled: false
-    }
-  ],
+/**
+ * @class
+ */
+module.exports = Seed.extend ({
+  model () {
+    return {
+      native: [
+        {
+          name: 'client1',
+          email: 'client1@gatekeeper.com',
+          client_secret: 'client1'
+        },
+        {
+          name: 'client2',
+          email: 'client2@gatekeeper.com',
+          client_secret: 'client2'
+        },
+        {
+          name: 'client3',
+          email: 'client3@gatekeeper.com',
+          client_secret: 'client3',
+          enabled: false
+        }
+      ],
 
-  android: [
-    {
-      name: 'android1',
-      client_secret: 'android1',
-      email: 'android1@gatekeeper.com',
-      package: 'com.onehilltech.gatekeeper',
-    }
-  ],
+      android: [
+        {
+          name: 'android1',
+          client_secret: 'android1',
+          email: 'android1@gatekeeper.com',
+          package: 'com.onehilltech.gatekeeper',
+        }
+      ],
 
-  recaptcha: [
-    {
-      name: 'recaptcha1',
-      recaptcha_secret: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe',
-      email: 'james@onehilltech.com',
-      scope: ['gatekeeper.account.create'],
-      origin: 'http://localhost'
-    },
+      recaptcha: [
+        {
+          name: 'recaptcha1',
+          recaptcha_secret: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe',
+          email: 'james@onehilltech.com',
+          scope: ['gatekeeper.account.create'],
+          origin: 'http://localhost'
+        },
 
-    {
-      name: 'recaptcha2',
-      recaptcha_secret: 'invalid-secret',
-      email: 'james@onehilltech.com',
-      scope: ['gatekeeper.account.create'],
-      origin: 'http://localhost'
-    }
-  ],
+        {
+          name: 'recaptcha2',
+          recaptcha_secret: 'invalid-secret',
+          email: 'james@onehilltech.com',
+          scope: ['gatekeeper.account.create'],
+          origin: 'http://localhost'
+        }
+      ],
 
-  accounts: [
-    {
-      email: 'hilljh82@gmail.com',
-      username: 'account1',
-      password: 'account1',
-    },
-    {
-      email: 'account2@gatekeeper.com',
-      username: 'account2',
-      password: 'account2',
-    },
-    {
-      email: 'account3@gatekeeper.com',
-      username: 'account3',
-      password: 'account3',
-    },
-    {
-      email: 'account4@gatekeeper.com',
-      username: 'account4',
-      password: 'account4',
-      scope: ['gatekeeper.account.update.scope']
-    },
-    {
-      email: 'account5@gatekeeper.com',
-      username: 'account5',
-      password: 'account5',
-      enabled: false
-    },
-    {
-      email: 'account6@gatekeeper.com',
-      username: 'account6',
-      password: 'account6',
-      enabled: true,
-      verification: {
-        date: new Date (),
-        ip_address: '127.0.0.1'
-      }
-    }
+      accounts: [
+        {
+          email: 'hilljh82@gmail.com',
+          username: 'account1',
+          password: 'account1',
+        },
+        {
+          email: 'account2@gatekeeper.com',
+          username: 'account2',
+          password: 'account2',
+        },
+        {
+          email: 'account3@gatekeeper.com',
+          username: 'account3',
+          password: 'account3',
+        },
+        {
+          email: 'account4@gatekeeper.com',
+          username: 'account4',
+          password: 'account4',
+          scope: ['gatekeeper.account.update.scope']
+        },
+        {
+          email: 'account5@gatekeeper.com',
+          username: 'account5',
+          password: 'account5',
+          enabled: false
+        },
+        {
+          email: 'account6@gatekeeper.com',
+          username: 'account6',
+          password: 'account6',
+          enabled: true,
+          verification: {
+            date: new Date (),
+            ip_address: '127.0.0.1'
+          }
+        }
 
-  ],
+      ],
 
-  user_tokens: [
-    { client: dab.ref ('native.0'), account: dab.ref ('accounts.0'), scope: ['gatekeeper.account.*'], refresh_token: dab.id () },
-    { client: dab.ref ('native.0'), account: dab.ref ('accounts.0'), scope: ['gatekeeper.account.get_all'], refresh_token: dab.id () },
-    { client: dab.ref ('native.0'), account: dab.ref ('accounts.0'), scope: ['dummy'], refresh_token: dab.id () },
-    { client: dab.ref ('native.1'), account: dab.ref ('accounts.0'), enabled: false, refresh_token: dab.id () },
-    { client: dab.ref ('native.2'), account: dab.ref ('accounts.0'), refresh_token: dab.id ()},
-    { client: dab.ref ('native.0'), account: dab.ref ('accounts.4'), refresh_token: dab.id ()},
-    { client: dab.ref ('recaptcha.0'), account: dab.ref ('accounts.0'), refresh_token: dab.id ()},
-    { client: dab.ref ('android.0'), account: dab.ref ('accounts.0'), refresh_token: dab.id ()}
-  ],
+      user_tokens: [
+        { client: dab.ref ('native.0'), account: dab.ref ('accounts.0'), scope: ['gatekeeper.account.*'], refresh_token: dab.id () },
+        { client: dab.ref ('native.0'), account: dab.ref ('accounts.0'), scope: ['gatekeeper.account.get_all'], refresh_token: dab.id () },
+        { client: dab.ref ('native.0'), account: dab.ref ('accounts.0'), scope: ['dummy'], refresh_token: dab.id () },
+        { client: dab.ref ('native.1'), account: dab.ref ('accounts.0'), enabled: false, refresh_token: dab.id () },
+        { client: dab.ref ('native.2'), account: dab.ref ('accounts.0'), refresh_token: dab.id ()},
+        { client: dab.ref ('native.0'), account: dab.ref ('accounts.4'), refresh_token: dab.id ()},
+        { client: dab.ref ('recaptcha.0'), account: dab.ref ('accounts.0'), refresh_token: dab.id ()},
+        { client: dab.ref ('android.0'), account: dab.ref ('accounts.0'), refresh_token: dab.id ()}
+      ],
 
-  client_tokens: [
-    { client: dab.ref ('native.0'), scope: ['gatekeeper.account.create']},
-    { client: dab.ref ('native.0'), scope: ['dummy']}
-  ]
-};
+      client_tokens: [
+        { client: dab.ref ('native.0'), scope: ['gatekeeper.account.create']},
+        { client: dab.ref ('native.0'), scope: ['dummy']}
+      ]
+    };
+  }
+});
