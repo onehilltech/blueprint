0f4014b874506b3891a407c545d9effa14f0c0fd
James H. Hill <hilljh82@gmail.com>
2018-07-03 00:46:20 -0400

    Added generator for seeds

---
+++ packages/blueprint-mongodb/cli/generators/mongodb:seed/files/app/seeds/mongodb/{{entityName}}.js
@@ -0,0 +1,16 @@
+const { Seed } = require ('@onehilltech/blueprint-mongodb');
+const dab = require ('@onehilltech/dab');
+
+/**
+ * @class
+ *
+ * The implementation of the seed definition. The seed definition must implement the
+ * model() function. It must return a dab definition.
+ */
+module.exports = new Seed ({
+  model () {
+    return {
+
+    }
+  }
+});
