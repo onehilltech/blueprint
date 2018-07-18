65838e67e41b204f143ed0e3d0f75bfe8c7ab1a1
James H. Hill <hilljh82@gmail.com>
2018-07-18 17:42:47 -0400

    Replaced protocols configuration property with connections configuration property.

--- examples/01_helloworld/app/configs/server.js
+++ examples/01_helloworld/app/configs/server.js
@@ -1,12 +1,26 @@
 const blueprint = require ('@onehilltech/blueprint');
 
 module.exports = {
+  // using protocols is the legacy method configuring server ports
   protocols : {
     http : {
       port: 5000
     },
     https : {
       port : 5001,
+      options : {
+        key  : blueprint.asset ('ssl/helloworld.key'),
+        cert : blueprint.asset ('ssl/helloworld.crt')
+      }
+    }
+  },
+
+  connections : {
+    insecure : { protocol: 'http', port: 8080 },
+
+    secure : {
+      port: 8443,
+      protocol: 'https',
       options : {
         key  : blueprint.asset ('ssl/helloworld.key'),
         cert : blueprint.asset ('ssl/helloworld.crt')
--- tests/unit-tests/lib/server/protocols/http-test.js
+++ tests/unit-tests/lib/server/protocols/http-test.js
@@ -1,25 +1,17 @@
 const {expect} = require ('chai');
+
+const blueprint = require ('../../../../../lib');
 const Http = require ('../../../../../lib/server/protocols/http');
 const http = require ('http');
 
 describe ('lib | server | protocols | Http', function () {
   describe ('createProtocol', function () {
     it ('should create a http protocol with default options', function () {
-      const options = {};
-      let p = Http.createProtocol ((req, res) => { }, options);
+      const { server: { connections: { insecure } } } = blueprint.app;
 
-      expect (p).to.be.instanceof (Http);
-      expect (p).to.have.deep.property ('options', {port: 80});
-      expect (p).to.have.property ('server').instanceof (http.Server);
-    });
-
-    it ('should create a http protocol with custom options', function () {
-      const options = {host: 'localhost', port: 8080};
-      let p = Http.createProtocol ((req, res) => { }, options);
-
-      expect (p).to.be.instanceof (Http);
-      expect (p).to.have.deep.property ('options', options);
-      expect (p).to.have.property ('server').instanceof (http.Server);
+      expect (insecure).to.be.instanceof (Http);
+      expect (insecure).to.have.property ('options').to.deep.include ({port: 8080});
+      expect (insecure).to.have.property ('server').instanceof (http.Server);
     });
   });
 });
