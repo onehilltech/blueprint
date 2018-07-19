aa0a41cced0453e8aab6ee6a6d31776cb500150e
James H. Hill <hilljh82@gmail.com>
2018-07-18 20:46:15 -0400

    Updated dependencies

--- packages/blueprint-cli/templates/blueprint-new/tests/unit-tests/bootstrap.js
+++ packages/blueprint-cli/templates/blueprint-new/tests/unit-tests/bootstrap.js
@@ -1,19 +1,5 @@
 const path  = require ('path');
-const blueprint = require ('@onehilltech/blueprint');
+const blueprint = require ('@onehilltech/blueprint-testing');
 
-before (function () {
-  const appPath = path.resolve (__dirname, '../../app');
-  return blueprint.createApplicationAndStart (appPath);
-});
-
-beforeEach (function () {
-  return blueprint.emit ('blueprint.test.start')
-});
-
-afterEach (function () {
-  return blueprint.emit ('blueprint.test.complete');
-});
-
-after (function () {
-  return blueprint.destroyApplication ();
-});
+const appPath = path.resolve (__dirname, '../../app');
+blueprint.bootstrap (appPath);
