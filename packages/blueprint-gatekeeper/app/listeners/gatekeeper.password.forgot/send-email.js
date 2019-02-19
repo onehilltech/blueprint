19a57c5760e9f2292b31369d46f29b0628da45e8
James H. Hill <hilljh82@gmail.com>
2019-02-18 21:01:41 -0500

    fix: The reset password email did not send an email

--- packages/blueprint-gatekeeper/app/listeners/gatekeeper.password.forgot/send-email.js
+++ packages/blueprint-gatekeeper/app/listeners/gatekeeper.password.forgot/send-email.js
@@ -1,97 +1,101 @@
-const blueprint = require ('@onehilltech/blueprint');
-const messaging = blueprint.messaging;
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const { Listener, computed, service } = require ('@onehilltech/blueprint');
 const path  = require ('path');
-const async = require ('async');
 const Email = require ('email-templates');
-const _ = require ('lodash');
+const {omit, merge, defaultsDeep} = require ('lodash');
 
 // TODO Construct the real location of the template directory.
 let templateDir = path.resolve (__dirname, '../../resources/email');
-let email = null;
 
 const DEFAULT_STYLE = {
   primaryColor: '#2196F3'
 };
 
-let appConfig;
-let gatekeeperConfig;
-let tokenGenerator;
-
-/**
- * app.init
- */
-blueprint.on ('app.init', function (app) {
-  if (!app.configs.app)
-    throw new Error ('app.config not defined');
-
-  if (!app.configs.gatekeeper)
-    throw new Error ('gatekeeper.config not defined');
-
-  // Save the Gatekeeper configuration.
-  appConfig = app.configs.app;
-  gatekeeperConfig = app.configs.gatekeeper || {};
-
-  if (!gatekeeperConfig.email)
-    return console.warn ('gatekeeper.config.js: no email configuration; cannot send reset password emails');
-
-  // Create the email template for the activation email while preventing the
-  // application from changing the location of the email templates.
-  let defaults = {
-    views: {
-      options: {
-        extension: 'ejs'
-      }
+const DEFAULT_EMAIL_OPTIONS = {
+  views: {
+    options: {
+      extension: 'ejs'
     }
-  };
-
-  let overrides = {
-    views: {
-      root: templateDir
-    }
-  };
-
-  let opts = _.merge (defaults, _.omit (gatekeeperConfig.email, ['locals']), overrides);
-
-  email = new Email (opts);
-  tokenGenerator = new ResetPasswordTokenGenerator ();
-});
-
-/**
- * Send the activation email to the account.
- */
-module.exports = function (account) {
-  // Do not continue if we have no email transport.
-  if (!email || !gatekeeperConfig.activationRequired)
-    return;
-
-  async.waterfall ([
-    function (callback) {
-      let payload = {email: account.email};
-      let options = {expiresIn: '10m'};
-
-      tokenGenerator.generateToken (payload, options, callback);
-    },
-
-    function (token, callback) {
-      let opts = {
-        template: 'gatekeeper.password.reset',
-        message: {
-          to: account.email
-        },
-        locals: _.defaultsDeep ({
-          appName: appConfig.name,
-          url: gatekeeperConfig['reset-password'].url,
-          email: account.email,
-          token
-        }, {style: DEFAULT_STYLE})
-      };
-
-      email.send (opts).then (value => callback (null, value)).catch (err => callback (err));
-    },
-
-    function (info, callback) {
-      messaging.emit ('gatekeeper.password.reset.email-sent', account, info);
-      return callback (null);
-    }
-  ]);
+  }
 };
+
+const OVERRIDE_EMAIL_OPTIONS = {
+  views: {
+    root: templateDir
+  }
+};
+
+/**
+ * Send an activation email to the newly created account.
+ */
+module.exports = Listener.extend ({
+  gatekeeper: service (),
+
+  _email: null,
+  _tokenGenerator: null,
+
+  init () {
+    this._super.call (this, ...arguments);
+
+    this._appConfig = this.app.lookup ('config:app');
+    this._gatekeeperConfig = this.app.lookup ('config:gatekeeper');
+
+    if (!this._gatekeeperConfig.email)
+      return console.warn ('gatekeeper: no email configured; cannot send password reset emails');
+
+    this._tokenGenerator = this.gatekeeper.getTokenGenerator ('gatekeeper:password_reset');
+
+    // Create the email template for the activation email while preventing the
+    // application from changing the location of the email templates.
+    let opts = merge (DEFAULT_EMAIL_OPTIONS, omit (this._gatekeeperConfig.email, ['locals']), OVERRIDE_EMAIL_OPTIONS);
+
+    this._email = new Email (opts);
+  },
+
+  hasEmail: computed ({
+    get () { return !!this._gatekeeperConfig.email }
+  }),
+
+  handleEvent (account) {
+    if (!this.hasEmail)
+      return;
+
+    // Generate an activation token. This token will be added to the email.
+    let payload = {email: account.email};
+    let options = {expiresIn: '10m'};
+
+    return this._tokenGenerator.generateToken (payload, options)
+      .then (token => {
+        let opts = {
+          template: 'gatekeeper.password.reset',
+          message: {
+            to: account.email
+          },
+          locals: defaultsDeep ({
+            appName: this._appConfig.name,
+            url: this._gatekeeperConfig['reset-password'].url,
+            email: account.email,
+            token
+          }, {style: DEFAULT_STYLE})
+        };
+
+        return this._email.send (opts);
+      })
+      .then (info => this.app.emit ('gatekeeper.email.password_reset.sent', account, info));
+  },
+});
