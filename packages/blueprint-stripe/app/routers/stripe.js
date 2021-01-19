7dda4a9a6960fbbb36f55da5f6c623e759d34f3d
James Hill <james@onehilltech.com>
2021-01-19 14:24:41 -0500

    chore: remove the stripe path from the router

--- packages/blueprint-stripe/app/routers/stripe.js
+++ packages/blueprint-stripe/app/routers/stripe.js
@@ -1,8 +1,6 @@
 module.exports = {
-  '/stripe': {
-    '/webhooks': {
-      //policy: check ('request.ips', blueprint.lookup ('config:stripe').ips),
-      post: { action: 'stripe@emitStripeEvent' }
-    }
+  '/webhooks': {
+    //policy: check ('request.ips', blueprint.lookup ('config:stripe').ips),
+    post: { action: 'stripe@emitStripeEvent' }
   }
 }
