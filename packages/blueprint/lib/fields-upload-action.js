f69320af9d6df994bc778ed8030544df817ce7dc
James Hill <james@onehilltech.com>
2021-12-31 13:44:39 -0500

    chore: ported the fields upload action

--- packages/blueprint/lib/fields-upload-action.js
+++ packages/blueprint/lib/fields-upload-action.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const assert       = require ('assert');
 const UploadAction = require ('./upload-action');
 
@@ -10,8 +26,11 @@ module.exports = UploadAction.extend ({
   /// The name of the field that will contain the uploaded file.
   fields: null,
 
-  init () {
-    this._super.call (this, ...arguments);
+  /**
+   * @override
+   */
+  async configure () {
+    await this._super.call (this, ...arguments);
 
     assert (!!this.fields, "You must define the 'fields' property.");
 
