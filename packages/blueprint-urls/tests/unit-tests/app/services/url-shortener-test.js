6188c961fd6fb02eef9c9f42c3d00906baedf3f9
James Hill <james@onehilltech.com>
2022-03-20 15:57:05 -0400

    chore: added more test cases

--- packages/blueprint-urls/tests/unit-tests/app/services/url-shortener-test.js
+++ packages/blueprint-urls/tests/unit-tests/app/services/url-shortener-test.js
@@ -6,17 +6,126 @@ function urlShortener () {
 }
 
 describe ('services | url-shortener', function () {
-  it ('should shorten a url', async function () {
-    const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share');
+  context ('no domain', function () {
+    it ('should shorten a url', async function () {
+      const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share');
 
-    expect (shortUrl.url).to.equal ('https://www.donatians.com/share');
-    expect (shortUrl.domain).to.be.undefined;
+      expect (shortUrl.url).to.equal ('https://www.donatians.com/share');
+      expect (shortUrl.domain).to.be.undefined;
+    });
+
+    it ('should redirect a short url', async function () {
+      const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share');
+
+      const req = {
+        baseUrl: shortUrl.short_code,
+        query: {}
+      };
+
+      const res = {
+        redirect (status, url) {
+          expect (status).to.equal (301);
+          expect (url).to.equal (shortUrl.url);
+        }
+      };
+
+      await urlShortener ().redirect (req, res);
+    });
+
+    it ('should fail because invalid short code', async function () {
+      const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share', { domain: 'demo'});
+
+      const req = {
+        baseUrl: '00000000',
+        query: { domain: 'invalid' }
+      };
+
+      const res = { };
+
+      try {
+        await urlShortener ().redirect (req, res);
+        expect.fail ('The redirect should have failed with NotFoundError');
+      }
+      catch (err) {
+        expect (err).to.include ({
+          name: 'NotFoundError',
+          code: 'not_found',
+          statusCode: 404
+        });
+      }
+    });
   });
 
-  it ('should shorten a url for a domain', async function () {
-    const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share', { domain: 'demo'} );
+  context ('domain', function () {
+    it ('should shorten a url for a domain', async function () {
+      const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share', { domain: 'demo'} );
 
-    expect (shortUrl.url).to.equal ('https://www.donatians.com/share');
-    expect (shortUrl.domain).to.equal ('demo');
+      expect (shortUrl.url).to.equal ('https://www.donatians.com/share');
+      expect (shortUrl.domain).to.equal ('demo');
+    });
+
+    it ('should redirect a short url', async function () {
+      const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share', { domain: 'demo'});
+
+      const req = {
+        baseUrl: shortUrl.short_code,
+        query: { domain: 'demo' }
+      };
+
+      const res = {
+        redirect (status, url) {
+          expect (status).to.equal (301);
+          expect (url).to.equal (shortUrl.url);
+        }
+      };
+
+      await urlShortener ().redirect (req, res);
+    });
+
+    it ('should fail because invalid short code', async function () {
+      const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share', { domain: 'demo'});
+
+      const req = {
+        baseUrl: '00000000',
+        query: { domain: 'demo' }
+      };
+
+      const res = { };
+
+      try {
+        await urlShortener ().redirect (req, res);
+        expect.fail ('The redirect should have failed with NotFoundError');
+      }
+      catch (err) {
+        expect (err).to.include ({
+          name: 'NotFoundError',
+          code: 'not_found',
+          statusCode: 404
+        });
+      }
+    });
+
+    it ('should fail to redirect because invalid domain', async function () {
+      const shortUrl = await urlShortener ().shorten ('https://www.donatians.com/share', { domain: 'demo'});
+
+      const req = {
+        baseUrl: shortUrl.short_code,
+        query: { domain: 'invalid' }
+      };
+
+      const res = { };
+
+      try {
+        await urlShortener ().redirect (req, res);
+        expect.fail ('The redirect should have failed with NotFoundError');
+      }
+      catch (err) {
+        expect (err).to.include ({
+          name: 'NotFoundError',
+          code: 'not_found',
+          statusCode: 404
+        });
+      }
+    });
   });
 });
