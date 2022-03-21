dcf0154bd42b6f6951ec2d539b1d503646240cc4
James Hill <james@onehilltech.com>
2022-03-20 23:33:29 -0400

    chore: added more comments

--- packages/blueprint-dfinity/examples/actors/helloworld.js
+++ packages/blueprint-dfinity/examples/actors/helloworld.js
@@ -1,17 +1,35 @@
 const { update, query, Actor } = require ('../lib');
 
+/**
+ * @class HelloWorldActor
+ *
+ * This example was borrowed from:
+ *
+ *   https://forum.dfinity.org/t/using-dfinity-agent-in-node-js/6169
+ */
 module.exports = Actor.extend ({
+  /// Define the greet update method.
   greet: update ('text', 'text'),
+
+  /// Define the green query method.
   greetq: query ('text', 'text'),
+
+  /// Define the whoami method.
   whoami: query (null, 'principal'),
 });
 
-/*
-const idlFactory = ({ IDL }) => {
-  return IDL.Service ({
-    'greet': IDL.Func([IDL.Text], [IDL.Text], []),
-    'greetq': IDL.Func([IDL.Text], [IDL.Text], ['query']),
-    'whoami': IDL.Func([], [IDL.Principal], ['query']),
-  });
-};
-*/
+/**
+ This version of the actor definition will be used once NodeJS supports decorators
+ by default.
+
+module.exports = class HelloActor extends Actor {
+  @update ('text', 'text')
+  greet;
+
+  @query ('text', 'text')
+  greetq;
+
+  @query (null, 'principal')
+  whoami;
+}
+ */
