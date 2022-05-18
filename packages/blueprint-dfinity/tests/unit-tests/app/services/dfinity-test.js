15ecaf32f68234f4c5a0623b0d89e23927538de1
James Hill <james@onehilltech.com>
2022-05-17 17:53:10 -0700

    chore: added more test cases

--- packages/blueprint-dfinity/tests/unit-tests/app/services/dfinity-test.js
+++ packages/blueprint-dfinity/tests/unit-tests/app/services/dfinity-test.js
@@ -1,7 +1,36 @@
+/*
+ * Copyright (c) 2022 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const blueprint = require ('@onehilltech/blueprint');
 const { expect } = require ('chai');
 
 describe ('app | services | dfinity', function () {
+  it ('should initialize the service', function () {
+    const dfinity = blueprint.lookup ('service:dfinity');
+
+    expect (dfinity.agents).to.have.keys (['$default']);
+    expect (dfinity.agents.$default._host.toString ()).to.equal ('http://localhost:8000/');
+
+    expect (dfinity.canisters).to.eql ({
+      $default: 'rrkah-fqaaa-aaaaa-aaaaq-cai'
+    });
+
+    expect (dfinity._idls).to.have.keys (['hello']);
+  });
+
   it ('should create a new instance', async function () {
     // We need to increase the timeout since there is a delay in connecting.
     this.timeout (10000);
