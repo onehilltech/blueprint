3e22a836b0ddeca70f869c774bfa13260bd12d62
James Hill <james@onehilltech.com>
2022-08-19 21:39:33 -0400

    chore: remove auto-generated code

--- packages/blueprint/tests/dummy/app/routers/inner/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '/main': {
-    get: {
-      action: 'main@performGet'
-    },
-    post: 'main'
-  }
-};
--- packages/blueprint/tests/dummy/app/routers/users.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '/users': {
-    resource: {
-      controller: 'namespace-user'
-    }
-  }
-};
