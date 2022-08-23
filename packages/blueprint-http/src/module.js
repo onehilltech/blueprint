2f9856e45a04005722a2870206ff36d012bd22b6
James Hill <james@onehilltech.com>
2022-08-23 17:48:39 -0400

    chore: define the resource types for the http module

---
+++ packages/blueprint-http/src/module.js
@@ -0,0 +1,11 @@
+/**
+ * Main entry point for the module.
+ *
+ * @param module
+ */
+module.exports = function (module) {
+  module.app.defineType ('model', { location: 'models' });
+  module.app.defineType ('policy', { location: 'policies' });
+  module.app.defineType ('controller', { location: 'controllers' });
+  module.app.defineType ('router', { location: 'routers' });
+};
