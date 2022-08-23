55c5ef325620e76610b623118b9e4c3b06fc1d65
James Hill <james@onehilltech.com>
2022-08-23 18:20:47 -0400

    chore: added return values to handle-error

--- packages/blueprint-http/lib/server/handle-error.js
+++ packages/blueprint-http/lib/server/handle-error.js
@@ -29,7 +29,7 @@ module.exports = function (err, req, res, next) {
       // We are working with an instance of a Blueprint error. This means that we have
       // can sent an error object to the client. If the error is a HttpError, then
       // the error object contains the status code as well.
-      let data = {
+      const data = {
         errors: [{code: err.code, detail: err.message }]
       };
 
@@ -47,18 +47,18 @@ module.exports = function (err, req, res, next) {
       if (err.details)
         data.errors[0].meta = err.details;
 
-      res.json (data);
+      return res.json (data);
     }
     else if (err instanceof Error) {
       // If this is a plan error object from JavaScript, then the only attribute
       // guaranteed is the message attribute.
-      res.status (500).send ({errors: [{detail: err.message}]});
+      return res.status (500).send ({errors: [{detail: err.message}]});
     }
     else {
       // This is just a regular object. We are going to set the meta
       // attribute on the returned error object.
 
-      res.status (500).send ({errors: [{meta: err}]});
+      return res.status (500).send ({errors: [{meta: err}]});
     }
   }
 };
